3df311b09fda99cfc5effe17764d0dee71061788
Christopher Swenson <crswenson@google.com>
2021-10-25 11:32:26 -0500

    Temporary fix for access denied for job errors

--- packages/malloy/src/db/bigquery.ts
+++ packages/malloy/src/db/bigquery.ts
@@ -535,11 +535,24 @@ export class BigQuery {
         ...createQueryJobOptions,
       });
 
-      const result = await job.getQueryResults({
-        timeoutMs: 1000 * 60 * 10,
-        ...getQueryResultsOptions,
-      });
-      return result;
+      // We do a simple retry-loop here, as a temporary fix for a transient
+      // error in which sometimes requesting results from a job yields an
+      // access denied error. It seems that in these cases, simply trying again
+      // solves the problem. This is being currently investigated by
+      // @christopherswenson and @lloydtabb.
+      let lastFetchError;
+      for (let retries = 0; retries < 3; retries++) {
+        try {
+          return await job.getQueryResults({
+            timeoutMs: 1000 * 60 * 10,
+            ...getQueryResultsOptions,
+          });
+        } catch (fetchError) {
+          lastFetchError = fetchError;
+        }
+      }
+
+      throw lastFetchError;
     } catch (e) {
       throw maybeRewriteError(e);
     }
