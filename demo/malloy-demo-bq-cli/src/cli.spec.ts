558566d6d958aa26892385608d791ca5b7c116d1
lloydtabb <ltabb@google.com>
2021-11-08 22:17:59 +0000

    Maybe the last one?

--- demo/malloy-demo-bq-cli/src/cli.spec.ts
+++ demo/malloy-demo-bq-cli/src/cli.spec.ts
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
-import { InMemoryUrlReader } from "malloy";
+import { InMemoryUrlReader } from "@malloy-lang/malloy";
 import { run, pathToUrl } from "./index";
 
 const unModeledQuery =
