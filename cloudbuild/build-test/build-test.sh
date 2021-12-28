cf177d8d9627d648668326006dd40134e990f93a
lloyd tabb <ltabb@google.com>
2021-12-28 14:14:42 -0800

    not sure wher bash is, run it without a path

--- cloudbuild/build-test/build-test.sh
+++ cloudbuild/build-test/build-test.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 nix-shell --pure --command "$(cat <<NIXCMD
   export PGHOST=localhost
   cd postgres_test
-  ./postgres_init.sh
+  bash postgres_init.sh
   cd ..
   yarn install
   yarn build
