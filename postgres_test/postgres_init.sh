36f8e7d3819e26b29d67c7fab471e92dcb4edb18
lloyd tabb <ltabb@google.com>
2021-12-29 04:53:08 -0800

    whoops

--- postgres_test/postgres_init.sh
+++ postgres_test/postgres_init.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 export PGHOST=localhost
+pg_ctl -D .tmp/data/malloytestdb -l .tmp/logfile -o "--unix_socket_directories='$PWD/.tmp'" stop
+rm -rf .tmp
 mkdir .tmp
 initdb -d .tmp/data/malloytestdb --no-locale --encoding=UTF8
 pg_ctl -D .tmp/data/malloytestdb -l .tmp/logfile -o "--unix_socket_directories='$PWD/.tmp'" start
