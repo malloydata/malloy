e7a7ac8842f082740e383cf5876dd2d815f75961
Ben Porterfield <bporterfield@google.com>
2022-01-07 10:40:10 -0800

    try using the service account user?

--- cloudbuild/build-test/build-test.sh
+++ cloudbuild/build-test/build-test.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 nix-shell --pure --command "$(cat <<NIXCMD
   export PGHOST=127.0.0.1
   export PGDATABASE=postgres
+  export PGUSER=673673622326@cloudbuild.gserviceaccount.com
   cd /workspace
   yarn install
   yarn build
