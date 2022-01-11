6e45dc0a07e3b010e63d365ddee0ec36b6281107
Michael Toy <mtoy@google.com>
2022-01-10 14:55:41 -1000

    restore production model test

--- packages/malloy/src/lang/production-models.spec.ts
+++ packages/malloy/src/lang/production-models.spec.ts
@@ -25,43 +25,41 @@ describe(`compiling server models`, () => {
     const projectPath = path.join(SAMPLE_PROJECT_ROOT, dir);
     if (!fs.statSync(projectPath).isDirectory()) continue;
     for (const fn of fs.readdirSync(projectPath)) {
-      if (fn.endsWith("_beta.malloy")) {
-        modelsFound = true;
-        const filePath = path.join(projectPath, fn);
-        const srcURI = `model://${filePath}`;
-        test(`checking ${srcURI}`, async () => {
-          const src = {
-            urls: { [srcURI]: fs.readFileSync(filePath, "utf-8") },
-          };
-          const trans = new MalloyTranslator(srcURI, src);
-          expect(trans).toBeValidMalloy();
-          let tr: TranslateResponse;
-          do {
-            tr = trans.translate();
-            if (tr.tables) {
-              throw new Error(
-                "Can't translate a model because it references a table"
+      modelsFound = true;
+      const filePath = path.join(projectPath, fn);
+      const srcURI = `model://${filePath}`;
+      test(`checking ${srcURI}`, async () => {
+        const src = {
+          urls: { [srcURI]: fs.readFileSync(filePath, "utf-8") },
+        };
+        const trans = new MalloyTranslator(srcURI, src);
+        expect(trans).toBeValidMalloy();
+        let tr: TranslateResponse;
+        do {
+          tr = trans.translate();
+          if (tr.tables) {
+            throw new Error(
+              "Can't translate a model because it references a table"
+            );
+            // const tables = await Malloy.db.getSchemaForMissingTables(
+            //   tr.tables
+            // );
+            // trans.update({ tables });
+          } else if (tr.urls) {
+            const files: { [fileName: string]: string } = {};
+            for (const neededFile of tr.urls) {
+              files[neededFile] = fs.readFileSync(
+                neededFile.replace("model://", ""),
+                "utf-8"
               );
-              // const tables = await Malloy.db.getSchemaForMissingTables(
-              //   tr.tables
-              // );
-              // trans.update({ tables });
-            } else if (tr.urls) {
-              const files: { [fileName: string]: string } = {};
-              for (const neededFile of tr.urls) {
-                files[neededFile] = fs.readFileSync(
-                  neededFile.replace("model://", ""),
-                  "utf-8"
-                );
-              }
-              trans.update({ urls: files });
-            } else {
-              expect(trans).toBeErrorless();
-              expect(tr).toHaveProperty("translated");
             }
-          } while (!tr.translated);
-        });
-      }
+            trans.update({ urls: files });
+          } else {
+            expect(trans).toBeErrorless();
+            expect(tr).toHaveProperty("translated");
+          }
+        } while (!tr.translated);
+      });
     }
   }
   expect(modelsFound).toBeTruthy();
