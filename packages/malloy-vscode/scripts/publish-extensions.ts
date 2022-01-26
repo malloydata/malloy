aedb23bf9701484d9992044f08b0cb064a78734d
Ben Porterfield <bporterfield@google.com>
2022-01-25 18:25:09 -0800

    files!

---
+++ packages/malloy-vscode/scripts/publish-extensions.ts
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+/* eslint-disable no-console */
+
+import { doBuild, outDir } from "./build.config";
+import * as semver from "semver";
+import * as fs from "fs";
+import * as path from "path";
+
+const args = process.argv.slice(2);
+const version = args[0];
+console.log(version);
+if (!semver.valid(version)) {
+  console.error("invalid version number");
+  process.exit(1);
+}
+
+async function publishExtensions() {
+  // const targets = [
+  //   "linux-x64",
+  //   "linux-arm64",
+  //   "linux-armhf",
+  //   "alpine-x64",
+  //   "alpine-arm64",
+  //   "darwin-x64",
+  //   "darwin-arm64",
+  // ];
+
+  const targets = [["darwin-x64", "keytar-v7.7.0-napi-v3-darwin-x64"]];
+
+  for (const [target, filename] of targets) {
+    await doBuild();
+
+    // copy target-specific native extension into the dist/ folder. esbuild has already moved the
+    // empty keytar.node file and renamed it. first, we have to find it.
+    const keytarFile = fs
+      .readdirSync(outDir)
+      .filter((fn) => fn.startsWith("keytar-"));
+
+    console.log(keytarFile);
+
+    // fs.copyFileSync(
+    //   path.join(
+    //     "..",
+    //     "..",
+    //     "third_party",
+    //     "github.com",
+    //     "atom",
+    //     "node-keytar",
+    //     filename
+    //   ),
+    //   path.join(outDir, "blah")
+    // );
+  }
+}
+
+publishExtensions();
