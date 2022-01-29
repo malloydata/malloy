dc2bcdfb97b9889072953442e041ff84354d5dd4
Ben Porterfield <bporterfield@google.com>
2022-01-29 13:09:15 -0800

    actual pre-release deploy;

--- cloudbuild/deploy/deploy-prerelease-extension.sh
+++ cloudbuild/deploy/deploy-prerelease-extension.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 nix-shell --pure --command "$(cat <<NIXCMD
   cd /workspace
   export VSCE_PAT=builtins.getEnv "VSCE_PAT"
-  echo $VSCE_PAT
+  yarn install --frozen-lockfile
+  yarn build && yarn vscode-publish-extensions pre-release
 NIXCMD
 )"
