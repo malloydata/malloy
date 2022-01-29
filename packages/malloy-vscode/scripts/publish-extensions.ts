f48f14b6b09cc3d5f653f35a637424a1c77bf7ee
Ben Porterfield <bporterfield@google.com>
2022-01-28 17:32:07 -0800

    double yay

--- packages/malloy-vscode/scripts/publish-extensions.ts
+++ packages/malloy-vscode/scripts/publish-extensions.ts
@@ -17,7 +17,18 @@ import { publishVSIX } from "vsce";
 import { Target, targetKeytarMap } from "./build-extension";
 import { doPackage } from "./package-extension";
 
-async function doPublish(version: string, preRelease = false) {
+async function doPublish(version: string) {
+  let preRelease = false;
+  if (version == "pre-release") {
+    // NOTE: pre-release is pinned to 0.1 here. We'll need to increment the minor if we
+    // ever push production version > 0.0.x
+    version = `0.1.${Math.floor(Date.now() / 1000)}`;
+    preRelease = true;
+  } else {
+    if (version && !semver.valid(version))
+      throw new Error(`Invalid semver: ${version}`);
+  }
+
   for (const target in targetKeytarMap) {
     const packagePath = await doPackage(target as Target, version, preRelease);
 
@@ -31,16 +42,13 @@ async function doPublish(version: string, preRelease = false) {
 }
 
 const args = process.argv.slice(2);
-
 const version = args[0];
 if (!version)
   throw new Error(
-    "No version passed to publish script. Call it with a semver version"
+    "No version passed to publish script. Call it with a semver version or pass 'pre-release'."
   );
 
-if (!semver.valid(version)) throw new Error(`Invalid semver: ${version}`);
-
-console.log(`Publishing extension version ${version}`);
+console.log(`Publishing extensions with version ${version}`);
 
 doPublish(version)
   .then(() => {
