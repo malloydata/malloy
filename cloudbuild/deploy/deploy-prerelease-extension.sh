2cb144808d9f4ba0c76a360a75d0f058c9fd3138
Ben Porterfield <bporterfield@google.com>
2022-01-29 14:03:15 -0800

    le sigh. nix is barely documented as far as I can tell, maybe this works

--- cloudbuild/deploy/deploy-prerelease-extension.sh
+++ cloudbuild/deploy/deploy-prerelease-extension.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --command "$(cat <<NIXCMD
+nix-shell --pure --keep VSCE_PAT --command  "$(cat <<NIXCMD
   cd /workspace
-  export VSCE_PAT=builtins.getEnv "VSCE_PAT"
   yarn install --frozen-lockfile
   yarn build && yarn vscode-publish-extensions pre-release
 NIXCMD
