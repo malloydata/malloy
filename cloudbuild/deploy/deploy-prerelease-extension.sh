4b5b933ca31cf00f31ca31e55e7d498e0521e976
Ben Porterfield <bporterfield@google.com>
2022-01-29 10:53:07 -0800

    test

--- cloudbuild/deploy/deploy-prerelease-extension.sh
+++ cloudbuild/deploy/deploy-prerelease-extension.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-$VSCE_PAT
+nix-shell --pure --command "$(cat <<NIXCMD
+  cd /workspace
+  export $VSCE_PAT = builtins.getEnv "VSCE_PAT"
+  echo $VSCE_PAT
+NIXCMD
+)"
