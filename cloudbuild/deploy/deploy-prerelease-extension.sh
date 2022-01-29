f3449fb3b47b465177a276afa74d9072b4d82275
Ben Porterfield <bporterfield@google.com>
2022-01-29 10:59:20 -0800

    ok

--- cloudbuild/deploy/deploy-prerelease-extension.sh
+++ cloudbuild/deploy/deploy-prerelease-extension.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 nix-shell --pure --command "$(cat <<NIXCMD
   cd /workspace
-  export VSCE_PAT = builtins.getEnv "VSCE_PAT"
+  export VSCE_PAT=builtins.getEnv "VSCE_PAT"
   echo $VSCE_PAT
 NIXCMD
 )"
