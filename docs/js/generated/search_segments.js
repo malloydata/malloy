aae239477cf17783434622ef78af33a4eccb9881
Christopher Swenson <crswenson@google.com>
2022-01-31 16:43:21 -0600

    Update docs

--- docs/js/generated/search_segments.js
+++ docs/js/generated/search_segments.js
@@ -3166,6 +3166,93 @@ window.SEARCH_SEGMENTS = [
   },
   {
     "titles": [
+      "Sample Models"
+    ],
+    "paragraphs": [
+      "Code snippets will appear throughout the Malloy documentation, many of which rely on a number of sample models based on publicly available datasets."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "FAA"
+    ],
+    "paragraphs": [
+      "This set of models points at a publicly available FAA flights dataset including information on flights, airports, aircrafts and aircraft models from 2000 to 2005. A wide variety of patterns and features are used in this model and any of our examples in documentation are based on this dataset, so it's a great place to start as you get to know Malloy."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "Iowa Liquor"
+    ],
+    "paragraphs": [],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "Ecommerce"
+    ],
+    "paragraphs": [
+      "This model points to a dataset for a fictitious ecommerce business. It has a  clean and typical schema for a transactional dataset. It also includes an example of an interesting brand affinity analysis (people who buy x also buy y)."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "GA Sessions"
+    ],
+    "paragraphs": [
+      "Malloy is ideally suited to working with nested data, and this is the place to see why. See how easily data at any level of nesting can be accessed and aggregated without needlessly complex queries or use of CTEs."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "Hacker News"
+    ],
+    "paragraphs": [],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "Names"
+    ],
+    "paragraphs": [
+      "A look at baby names in the United States by gender, state, and year, since 1910. Includes an example of cohorting names by aggregating safely across different levels of nesting."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "The Met"
+    ],
+    "paragraphs": [
+      "Looks at a catalog of over 200,000 public domain items from The Met (The Metropolitan Museum of Art). The catalog includes metadata about each piece of art, along with an image or images of the artifact."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
+      "Sample Models",
+      "Wordlebot"
+    ],
+    "paragraphs": [
+      "Liquor sales in Iowa are state-controlled, with all liquor wholesale run by the state. All purchases and sales of liquor that stores make are a matter of public record. A walkthrough of exploring and modeling this dataset can be found <a href=\"examples/iowa/iowa.html\">here</a>; this makes a great introduction to Malloy.",
+      "This is just a fun dataset. Includes examples of using regular expressions to parse data, <a href=\"language/expressions#pick-expressions\">pick</a> (Malloy's improvement upon CASE statements), and <a href=\"language/imports.html\">imports</a> to spin off a specific analysis of posts about FAANG companies.",
+      "Let Wordlebot solve Wordle for you (or if you're like us, see if it can beat you after you've played!). This is an example of an advanced analysis to solve a tricky problem. We have a walkthrough and examples of how we used the model to solve Wordle puzzles available <a href=\"examples/wordle/wordle.html\">here</a>."
+    ],
+    "path": "/samples.md"
+  },
+  {
+    "titles": [
       "Bar Charts"
     ],
     "paragraphs": [],
