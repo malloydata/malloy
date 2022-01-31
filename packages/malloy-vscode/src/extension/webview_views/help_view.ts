290e05ad8a406b910e9b6df6d5010b1ac3d3fbb8
Christopher Swenson <crswenson@google.com>
2022-01-31 17:06:38 -0600

    Remove some unused code

--- packages/malloy-vscode/src/extension/webview_views/help_view.ts
+++ packages/malloy-vscode/src/extension/webview_views/help_view.ts
@@ -54,44 +54,4 @@ export class HelpViewProvider implements vscode.WebviewViewProvider {
       }
     });
   }
-
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    // const scriptUri = webview.asWebviewUri(
-    //   vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
-    // );
-
-    // // Do the same for the stylesheet.
-    // const styleResetUri = webview.asWebviewUri(
-    //   vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-    // );
-    // const styleVSCodeUri = webview.asWebviewUri(
-    //   vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-    // );
-    // const styleMainUri = webview.asWebviewUri(
-    //   vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
-    // );
-
-    // Use a nonce to only allow a specific script to be run.
-    const nonce = "dfsd";
-
-    return `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <!--
-          Use a content security policy to only allow loading images from https or from our extension directory,
-          and only allow scripts that have a specific nonce.
-        -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Cat Colors</title>
-      </head>
-      <body>
-        <ul class="color-list">
-        </ul>
-        <button class="add-color-button">Add Color</button>
-      </body>
-      </html>`;
-  }
 }
