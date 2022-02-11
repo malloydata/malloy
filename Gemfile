8735853fd8786e2f6b3181667f83135e1bbbf56d
Christopher Swenson <crswenson@google.com>
2022-02-11 09:33:26 -0600

    Happy belated new year

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
--- packages/malloy-db-test/src/multi_connection.spec.ts
+++ packages/malloy-db-test/src/multi_connection.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
