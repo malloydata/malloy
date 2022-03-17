45b52d6f4756cd1ae7aa088d38a35944f4929282
lloydtabb <ltabb@google.com>
2022-03-17 01:50:32 +0000

    improve performance upload script.

---
+++ samples/imdb/upload_imdb.sh
@@ -0,0 +1,64 @@
+BUCKET=imdb_data_upload
+DATASET=imdb_test
+
+gsutil rm -r gs://$BUCKET
+gsutil mb gs://$BUCKET
+rm *.tsv *.gz
+
+for filename in name.basics titles.akas title.basics title.crew \
+  title.episode title.principals title.ratings 
+do 
+  wget https://datasets.imdbws.com/$filename.tsv.gz
+  gunzip $filename.tsv.gz
+  gsutil cp $filename.tsv gs://$BUCKET
+  bq load --skip_leading_rows=1 --replace=true --source_format=CSV  --quote="" \
+    --schema `head -1 $filename.tsv | sed '-e s/\t/,/g' `  \
+    --field_delimiter='\t' $DATASET.`echo $filename | sed -e 's/\./_/'`_raw gs://$BUCKET/$filename.tsv
+done
+
+rm *.tsv
+
+bq query --replace=true --use_legacy_sql=false --target_dataset=$DATASET --dataset_id=$DATASET <<- "EOF"
+  create or replace table name_basics as (
+    SELECT 
+      * except(primaryProfession, knownForTitles, birthYear),
+      NULLIF(safe_cast(birthYear as int64),0) birthYear,
+      ARRAY((SELECT AS STRUCT value  
+        FROM  UNNEST(split(primaryProfession,',')) as value )) as primaryProfession,
+      ARRAY((SELECT AS STRUCT value  
+        FROM  UNNEST(split(knownForTitles,',')) as value )) as knownForTitles
+    FROM name_basics_raw
+  );
+
+  create or replace table title_crew as (
+    SELECT 
+      * except(directors,writers),
+      ARRAY((SELECT AS STRUCT value  
+        FROM  UNNEST(split(directors,',')) as value )) as director_ids,
+      ARRAY((SELECT AS STRUCT value  
+        FROM  UNNEST(split(writers,',')) as value )) as writer_ids
+    FROM title_crew_raw
+  );
+
+  create or replace table title_ratings as (
+    SELECT 
+      tconst, 
+      cast(averageRating as float64) averageRaging,
+      cast(numVotes as int64) numVotes
+    FROM title_ratings_raw
+  );
+
+  create or replace table title_principals as (
+    SELECT * from title_principals_raw
+  );
+
+  create or replace table title_basics as (
+    SELECT 
+      * except(genres,startYear,endYear),
+      NULLIF(safe_cast(startYear as int64),0) startYear,
+      ARRAY((SELECT AS STRUCT value  
+        FROM  UNNEST(split(genres,',')) as value )) as genres
+    FROM title_basics_raw
+  );
+EOF
+
