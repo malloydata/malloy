70d71878fd13c3be469f84c1dbbe341720cf30ed
Ben Porterfield <bporterfield@google.com>
2022-05-14 14:10:56 -0700

    on path to using esbuild with composer demo - build composer demo with normal build, set up tsconfig properly (reference top-level one), move building and serving app to esbuild. requires more work for watching/debug. ooutstanding issue is that main logo is not loaded (pathing is incorrect).

---
+++ demo/malloy-demo-composer/scripts/serve.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+import { serve } from "esbuild";
+import inlineImage from "esbuild-plugin-inline-image";
+import svgrPlugin from "esbuild-plugin-svgr";
+
+async function doServe() {
+  await serve(
+    {
+      servedir: "public",
+      port: 3000,
+    },
+    {
+      entryPoints: ["./src/index.tsx"],
+      outfile: "./public/js/app.js",
+      minify: false,
+      bundle: true,
+      platform: "browser",
+      sourcemap: true,
+      loader: {
+        ".js": "jsx",
+      },
+      plugins: [
+        inlineImage({ extensions: ["png"] }),
+        svgrPlugin({ exportType: "named" }),
+      ],
+      define: {
+        "process.env.NODE_DEBUG": "false", // TODO this is a hack because some package we include assumed process.env exists :(
+      },
+      inject: ["./react-shim.js"], // This shim elimanites needing to have "require React from 'react'" in every file
+    }
+  ).catch((e: any) => {
+    console.log(e);
+    process.exit(1);
+  });
+}
+
+doServe();
