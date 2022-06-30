8e19d38b11b0ce63eee2be5992b91f4a2dfbea72
Christopher Swenson <crswenson@google.com>
2022-06-30 10:03:39 -0500

    Package darwin arm64

--- demo/malloy-demo-composer/scripts/build.ts
+++ demo/malloy-demo-composer/scripts/build.ts
@@ -22,6 +22,7 @@ import fs from "fs";
 export const targetDuckDBMap: Record<string, string> = {
   "linux-x64": "duckdb-v0.4.0-node-v93-linux-x64.node",
   "darwin-x64": "duckdb-v0.4.0-node-v93-darwin-x64.node",
+  "darwin-arm64": "duckdb-v0.4.0-node-v93-darwin-arm64.node",
 };
 
 export const buildDirectory = "build/";
