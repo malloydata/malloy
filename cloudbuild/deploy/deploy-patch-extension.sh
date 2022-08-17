bbe379233363ee03c7fb3504d6c2c88f45037a6a
Christopher Swenson <crswenson@google.com>
2022-08-17 11:33:13 -0500

    update cloudbuild script

--- cloudbuild/deploy/deploy-patch-extension.sh
+++ cloudbuild/deploy/deploy-patch-extension.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --keep VSCE_PAT --command  "$(cat <<NIXCMD
+nix-shell --pure --keep VSCE_PAT GA_API_SECRET GA_MEASUREMENT_ID --command  "$(cat <<NIXCMD
   cd /workspace
   yarn install --frozen-lockfile
   yarn build && yarn vscode-publish-extensions patch
