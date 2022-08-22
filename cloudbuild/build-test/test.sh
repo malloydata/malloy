04e6e21a2043b2bb6cd6d39384329ac87b20036f
Ben Porterfield <bporterfield@google.com>
2022-08-22 13:12:10 -0700

    try silent reporter for CI test runs

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -7,6 +7,6 @@ nix-shell --pure --command "$(cat <<NIXCMD
   export PGUSER=673673622326@cloudbuild
   cd /workspace
   npm ci
-  npm run lint && npm run build && npm run build-duckdb-db && npm run test
+  npm run lint && npm run build && npm run build-duckdb-db && npm run test-silent
 NIXCMD
 )"
