aed8c46d865d8595742f4c16f947a700f34032a0
Ben Porterfield <bporterfield@google.com>
2022-08-22 16:43:20 -0700

    quiet other things down for CI tests

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --command "$(cat <<NIXCMD
+nix-shell --quiet --pure --command "$(cat <<NIXCMD
   export PGHOST=127.0.0.1
   export PGDATABASE=postgres
   export PGUSER=673673622326@cloudbuild
