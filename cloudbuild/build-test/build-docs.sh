f91cf7544c9eb1c83092db50e752543e2f44965c
Ben Porterfield <bporterfield@google.com>
2022-08-22 17:35:05 -0700

    quiet down now

--- cloudbuild/build-test/build-docs.sh
+++ cloudbuild/build-test/build-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --command "$(cat <<NIXCMD
+nix-shell --quiet --pure --command "$(cat <<NIXCMD
   cd /workspace
   npm ci --silent
   bundle install
