853f14a299cdf18476dbb7068ce6803ef3a2dd1b
Ben Porterfield <bporterfield@users.noreply.github.com>
2022-08-23 13:17:12 -0700

    Cleanup ci output (#762)
    clean up CI output so that it's more legible

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --command "$(cat <<NIXCMD
+nix-shell --quiet --pure --command "$(cat <<NIXCMD
   export PGHOST=127.0.0.1
   export PGDATABASE=postgres
   export PGUSER=673673622326@cloudbuild
   cd /workspace
-  npm ci
-  npm run lint && npm run build && npm run build-duckdb-db && npm run test
+  npm ci --silent
+  npm run lint && npm run build && npm run build-duckdb-db && npm run test-silent
 NIXCMD
 )"
