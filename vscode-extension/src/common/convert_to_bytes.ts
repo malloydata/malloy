91744f60eb2daed8299d2434f5221113f9b8532a
Will Scullin <willscullin@google.com>
2022-10-13 14:07:11 -0700

    Add missing license headers (#869)

--- vscode-extension/src/common/convert_to_bytes.ts
+++ vscode-extension/src/common/convert_to_bytes.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 const BYTE_SUFFIXES = ["k", "m", "g", "t", "p"];
 const BYTE_MATCH = /^(?<bytes>\d+)((?<suffix>[kmgtp])((?<iec>i)?b)?)?$/i;
 
