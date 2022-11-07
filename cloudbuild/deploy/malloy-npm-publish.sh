dfb74cf990e16a2e2c337f12350f50a0ccf483fa
Will Scullin <willscullin@google.com>
2022-11-07 23:55:31 +0000

    Different approach

--- cloudbuild/deploy/malloy-npm-publish.sh
+++ cloudbuild/deploy/malloy-npm-publish.sh
@@ -3,18 +3,16 @@ set -euxo pipefail
 
 export PACKAGES="packages/malloy packages/malloy-db-bigquery packages/malloy-db-duckdb packages/malloy-db-postgres packages/malloy-render"
 
-nix-shell --pure --keep BRANCH_NAME --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
+nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --command "$(cat <<NIXCMD
   set -euxo pipefail
   cd /workspace
-  git fetch origin \$BRANCH_NAME
-  git checkout \$BRANCH_NAME
   npm --no-audit --no-fund ci --loglevel error
   npm run build
   echo Publishing \$PACKAGES
+  PRERELEASE=\$(date +%y%m%d%H%M%S)
   for package in \$PACKAGES; do
-    echo Publishing \$package
     VERSION=\$(jq -r .version \$package/package.json)
-    PRERELEASE=\$(git rev-list --count \$BRANCH_NAME)
+    echo Publishing \$package \$VERSION-\$PRERELEASE
     npm version -w \$package \$VERSION-\$PRERELEASE
     npm publish -w \$package --dry-run --tag next
   done
--- default.nix
+++ default.nix
@@ -1,1 +1,1 @@
-with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x jdk8 google-cloud-sdk postgresql ruby.devEnv git jq cacert fakeroot]; }
+with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x jdk8 google-cloud-sdk postgresql ruby.devEnv git jq fakeroot]; }
