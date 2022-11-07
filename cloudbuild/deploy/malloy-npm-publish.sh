9b17aa6eb3596d45f35f993bcf4136d7b5d2a3d6
Will Scullin <willscullin@google.com>
2022-11-07 22:33:01 +0000

    Use branch name

--- cloudbuild/deploy/malloy-npm-publish.sh
+++ cloudbuild/deploy/malloy-npm-publish.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 export PACKAGES="packages/malloy packages/malloy-db-bigquery packages/malloy-db-duckdb packages/malloy-db-postgres packages/malloy-render"
 
-nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
+nix-shell --pure --keep BRANCH_NAME --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
   set -euxo pipefail
   cd /workspace
   npm --no-audit --no-fund ci --loglevel error
@@ -12,7 +12,7 @@ nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
   for package in \$PACKAGES; do
     echo Publishing \$package
     VERSION=\$(jq -r .version \$package/package.json)
-    PRERELEASE=\$(git rev-list --count HEAD)
+    PRERELEASE=\$(git rev-list --count \$BRANCH_NAME)
     npm version -w \$package \$VERSION-\$PRERELEASE
     npm publish -w \$package --dry-run --tag next
   done
