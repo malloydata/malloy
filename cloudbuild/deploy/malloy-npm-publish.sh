b466f08e86c8b5a4e8337c020e84ad1efa328d6d
Will Scullin <willscullin@google.com>
2022-11-07 22:22:00 +0000

    handle errors, don't rely on branch name

--- cloudbuild/deploy/malloy-npm-publish.sh
+++ cloudbuild/deploy/malloy-npm-publish.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 export PACKAGES="packages/malloy packages/malloy-db-bigquery packages/malloy-db-duckdb packages/malloy-db-postgres packages/malloy-render"
 
 nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
+  set -euxo pipefail
   cd /workspace
   npm --no-audit --no-fund ci --loglevel error
   npm run build
@@ -11,7 +12,7 @@ nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --run "$(cat <<NIXCMD
   for package in \$PACKAGES; do
     echo Publishing \$package
     VERSION=\$(jq -r .version \$package/package.json)
-    PRERELEASE=\$(git rev-list --count main)
+    PRERELEASE=\$(git rev-list --count HEAD)
     npm version -w \$package \$VERSION-\$PRERELEASE
     npm publish -w \$package --dry-run --tag next
   done
