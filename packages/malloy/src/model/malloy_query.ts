dbd0b568ffbabf8a1742ee4a455633de606a3e8f
Christopher Swenson <crswenson@google.com>
2022-11-09 09:41:20 -0600

    QueryStruct doesn't need to be exported

--- packages/malloy/src/model/malloy_query.ts
+++ packages/malloy/src/model/malloy_query.ts
@@ -3209,7 +3209,7 @@ FROM ${resultStage}\n`
 }
 
 /** Structure object as it is used to build a query */
-export class QueryStruct extends QueryNode {
+class QueryStruct extends QueryNode {
   fieldDef: StructDef;
   parent: QueryStruct | undefined;
   model: QueryModel;
