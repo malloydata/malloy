fe564b1026992e3e505ea3277dfd95d3c175ecc8
Will Scullin <willscullin@google.com>
2022-11-10 12:13:40 -0600

    Version 0.0.8 (#928)

--- cloudbuild/deploy/malloy-npm-publish.sh
+++ cloudbuild/deploy/malloy-npm-publish.sh
@@ -10,10 +10,10 @@ nix-shell --pure --keep NPM_TOKEN --keep PACKAGES --command "$(cat <<NIXCMD
   npm run build
   echo Publishing \$PACKAGES
   PRERELEASE=\$(date +%y%m%d%H%M%S)
+  VERSION=\$(jq -r .version ./lerna.json)-dev\$PRERELEASE
+  npx lerna version \$VERSION --yes --no-push --no-git-tag-version
   for package in \$PACKAGES; do
-    VERSION=\$(jq -r .version \$package/package.json)
-    echo Publishing \$package \$VERSION-\$PRERELEASE
-    npm version -w \$package \$VERSION-\$PRERELEASE
+    echo Publishing \$package \$VERSION
     npm publish -w \$package --tag next
   done
 NIXCMD
