8270e1ee7e436c79df7b5435110ed64422f9bb96
Michael Aden <maden@google.com>
2023-02-14 13:51:36 -0600

    Update logging and some comments.

--- scripts/cloud/update_malloy.sh
+++ scripts/cloud/update_malloy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Install and update Malloy environment in Google Cloud Shell/IDE.
+#
 
 samples_repo=https://github.com/malloydata/malloy-samples.git
 samples_dir=malloy-samples
@@ -7,10 +10,24 @@ composer_latest_api=https://api.github.com/repos/malloydata/malloy-composer/rele
 # Save users initial directory
 starting_dir="$(pwd)"
 
+log() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+}
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
 ### Component Functions ###
+
+#######################################
+# Installs a fresh copy of the Malloy Extension.
+# Arguments:
+#   None
 # TODO(maden): Update this to be dynamic
+#######################################
 install_extension() {
-  echo "Installing Extension:"
+  log "Installing Extension:"
   extension_file="malloy-vscode-linux-x64-0.2.0.vsix"
 
   extension_url="https://www.scullinsteel.com/$extension_file"
@@ -18,12 +35,12 @@ install_extension() {
 
   extension_dir=${extension_file%.vsix}
 
-  echo "  Creating directory: ~/$theia_dir/$extension_dir"
+  log "  Creating directory: ~/$theia_dir/$extension_dir"
   cd ~
 
   if [ -d $theia_dir/$extension_dir ]; then
-    echo "  Found existing version..."
-    echo "  Removing existing version..."
+    log "  Found existing version..."
+    log "  Removing existing version..."
     rm -Rf $theia_dir/$extension_dir;
   fi
 
@@ -31,70 +48,90 @@ install_extension() {
 
   cd $theia_dir
 
-  echo "  Downloading Extension..."
+  log "  Downloading Extension..."
   curl $extension_url --output $extension_file
 
-  echo "  Unpacking Exension..."
+  log "  Unpacking Exension..."
   unzip -d $extension_dir $extension_file
 
-  echo "  Extension Installed Successfully"
+  log "  Extension Installed Successfully"
 }
 
+#######################################
+# Creates a new clone of the malloy-samples repository.
+# Arguments:
+#   None
 # TODO(maden): How to update, pull latest? nuke and reclone?
+#######################################
 install_samples() {
-  echo "Installing Samples:"
+  log "Installing Samples:"
 
   cd ~
-  echo "  Cloning samples repo from: $samples_repo"
+  log "  Cloning samples repo from: $samples_repo"
 
   if [ -d $samples_dir ]; then
-    echo "  Found existing samples..."
-    echo "  Removing existing samples...."
+    log "  Found existing samples..."
+    log "  Removing existing samples...."
     rm -Rf $samples_dir
   fi
 
   git clone $samples_repo
-  echo "  Samples Installed Successfully"
+  log "  Samples Installed Successfully"
 }
 
+#######################################
+# Downloads the latest release of composer from malloy-composer repo.
+# Arguments:
+#   None
+#######################################
 install_composer() {
-  echo "Installing Composer:"
+  log "Installing Composer:"
 
   cd ~
-  echo "  Fetching latest version..."
+  log "  Fetching latest version..."
   latest_url=$(curl -s $composer_latest_api | jq -r ".assets[] | select(.name|match(\"linux-x64.zip\")) | .browser_download_url")
   latest_zip=${latest_url##*/}
   version_base=${latest_url%/$latest_zip}
   version=${version_base##*/}
 
-  echo "  Latest version found: $version"
-  echo "  Pulling from: $latest_url"
+  log "  Latest version found: $version"
+  log "  Pulling from: $latest_url"
   curl -L $latest_url --output $latest_zip
 
-  echo "  Unpacking composer executable"
+  log "  Unpacking composer executable"
   unzip -p $latest_zip composer > composer
   chmod a+x composer
 
   rm $latest_zip
 
-  echo "  Composer Installed Successfully"
+  log "  Composer Installed Successfully"
 }
 
+#######################################
+# Updates theia workspace settings to set cloud project id.
+# Arguments:
+#   PROJECT_ID
+#######################################
 configure_cloud_project() {
-  echo "Configuring Cloud Project:"
+  log "Configuring Cloud Project:"
 
   cd ~
-  echo "  Updating workspace settings..."
+  log "  Updating workspace settings..."
   cat .theia/settings.json | jq '."cloudcode.cloudshell.project" = $project_id' --arg project_id $1 > settings.json
 
   if [ $? -eq 0 ]; then
     mv settings.json .theia/settings.json
-    echo "  Workspace Cloud Project ID has been updated"
+    log "  Workspace Cloud Project ID has been updated"
   else
-    echo "  An error occurred updating workspace settings"
+    err "  An error occurred updating workspace settings"
   fi
 }
 
+#######################################
+# Create a small update script in the home directory for updating malloy components.
+# Arguments:
+#   None
+#######################################
 write_update_script() {
   cd ~
   update_file=update_malloy.sh
