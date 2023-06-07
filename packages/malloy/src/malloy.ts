b4c72b5e860ebd68c4e51a1199d2d8dae1895e59
Fernando Arreola <figutierrez@google.com>
2023-06-07 22:34:50 +0000

    Clean up a bit further.

--- packages/malloy/src/malloy.ts
+++ packages/malloy/src/malloy.ts
@@ -412,6 +412,7 @@ export class Malloy {
           // TODO feature-sql-block There is no source explore...
           sourceExplore: '',
           sourceFilters: [],
+          // TODO: Figure out where to grab query timezone from in this case.
         },
         {
           name: 'empty_model',
