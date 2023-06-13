17d85d43f6a091251e904996d225f0217651ad6c
Fernando Arreola <figutierrez@google.com>
2023-06-13 18:22:27 +0000

    More cleaunup.

--- packages/malloy/src/dialect/duckdb.ts
+++ packages/malloy/src/dialect/duckdb.ts
@@ -314,7 +314,6 @@ export class DuckDBDialect extends Dialect {
     if (from.valueType === 'timestamp') {
       const tz = qtz(qi);
       if (tz) {
-        // ACTUAL USE
         extractFrom = mkExpr`(${extractFrom}::TIMESTAMPTZ AT TIME ZONE '${tz}')`;
       }
     }
@@ -371,7 +370,7 @@ export class DuckDBDialect extends Dialect {
     }
     const tz = timezone || qtz(qi);
     if (tz) {
-      return `TIMESTAMPTZb '${timeString} ${tz}'::TIMESTAMP`;
+      return `TIMESTAMPTZ '${timeString} ${tz}'::TIMESTAMP`;
     }
     return `TIMESTAMP '${timeString}'`;
   }
