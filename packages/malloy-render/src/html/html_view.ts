1e44bf3c6acd12e2c3a2b0ccf25000be2288b0b9
Fernando Arreola <figutierrez@google.com>
2023-06-23 19:34:30 +0000

    Update renderer html renderer to use tags.

--- packages/malloy-render/src/html/html_view.ts
+++ packages/malloy-render/src/html/html_view.ts
@@ -60,7 +60,8 @@ export class HTMLView {
 
   async render(
     table: DataArray,
-    options: RendererOptions
+    options: RendererOptions,
+    tagProperties: Record<string, string | boolean>
   ): Promise<HTMLElement> {
     const renderer = makeRenderer(
       table.field,
@@ -69,7 +70,8 @@ export class HTMLView {
       {
         size: 'large',
       },
-      table.field.structDef.queryTimezone
+      table.field.structDef.queryTimezone,
+      tagProperties
     );
     try {
       // TODO Implement row streaming capability for some renderers: some renderers should be usable
@@ -113,25 +115,29 @@ export class JSONView {
 }
 
 const suffixMap: Record<string, RenderDef['renderer']> = {
-  '_shape_map': 'shape_map',
-  '_point_map': 'point_map',
-  '_bar_chart': 'bar_chart',
-  '_image': 'image',
-  '_json': 'json',
-  '_segment_map': 'segment_map',
-  '_dashboard': 'dashboard',
-  '_line_chart': 'line_chart',
-  '_scatter_chart': 'scatter_chart',
-  '_url': 'link',
-  '_list': 'list',
-  '_list_detail': 'list_detail',
-  '_sparkline': 'sparkline',
-  '_sparkline_area': 'sparkline',
-  '_sparkline_column': 'sparkline',
-  '_sparkline_bar': 'sparkline',
+  'shape_map': 'shape_map',
+  'point_map': 'point_map',
+  'bar_chart': 'bar_chart',
+  'image': 'image',
+  'json': 'json',
+  'segment_map': 'segment_map',
+  'dashboard': 'dashboard',
+  'line_chart': 'line_chart',
+  'scatter_chart': 'scatter_chart',
+  'url': 'link',
+  'list': 'list',
+  'list_detail': 'list_detail',
+  'sparkline': 'sparkline',
+  'sparkline_area': 'sparkline',
+  'sparkline_column': 'sparkline',
+  'sparkline_bar': 'sparkline',
 };
 
-function getRendererOptions(field: Field | Explore, dataStyles: DataStyles) {
+function getRendererOptions(
+  field: Field | Explore,
+  dataStyles: DataStyles,
+  tagProperties: Record<string, string | boolean>
+) {
   let renderer = dataStyles[field.name];
   if (!renderer) {
     for (const sourceClass of field.sourceClasses) {
@@ -141,25 +147,38 @@ function getRendererOptions(field: Field | Explore, dataStyles: DataStyles) {
     }
   }
 
+  const {name} = field;
   for (const suffix in suffixMap) {
-    const {name} = field;
-    if (name.endsWith(suffix)) {
-      const label = name.slice(0, name.length - suffix.length);
-      if (renderer) {
-        renderer.renderer ??= suffixMap[suffix];
-        renderer.data ??= {};
-        renderer.data.label ??= label;
-      } else {
-        renderer = {
-          renderer: suffixMap[suffix],
-          data: {
-            label,
-          },
-        };
-      }
+    if (tagProperties[suffix] === true) {
+      return updateOrCreateRenderer(suffix, name, renderer);
+    }
 
+    if (name.endsWith(`_${suffix}`)) {
+      const label = name.slice(0, name.length - suffix.length - 1);
+      updateOrCreateRenderer(suffix, label, renderer);
       break;
     }
+  }
+
+  return renderer;
+}
+
+function updateOrCreateRenderer(
+  rendererKey: string,
+  label: string,
+  renderer?: RenderDef
+) {
+  if (renderer) {
+    renderer.renderer ??= suffixMap[rendererKey];
+    renderer.data ??= {};
+    renderer.data.label ??= label;
+  } else {
+    renderer = {
+      renderer: suffixMap[rendererKey],
+      data: {
+        label,
+      },
+    };
   }
 
   return renderer;
@@ -180,9 +199,11 @@ export function makeRenderer(
   document: Document,
   options: RendererOptions,
   styleDefaults: StyleDefaults,
-  queryTimezone: string | undefined
+  queryTimezone: string | undefined,
+  tagProperties: Record<string, string | boolean>
 ): Renderer {
-  const renderDef = getRendererOptions(field, options.dataStyles) || {};
+  const renderDef =
+    getRendererOptions(field, options.dataStyles, tagProperties) || {};
   options.dataStyles[field.name] = renderDef;
 
   if (renderDef.renderer === 'shape_map') {
@@ -373,7 +394,8 @@ function makeContainerRenderer<Type extends ContainerRenderer>(
       document,
       options,
       c.defaultStylesForChildren,
-      explore.queryTimezone
+      explore.queryTimezone,
+      field.getTags().getMalloyTags().properties
     );
   });
   c.childRenderers = result;
