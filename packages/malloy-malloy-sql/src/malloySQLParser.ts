05a3c94347cc83dfdd589540f7d4cba1d9ef6af3
Will Scullin <willscullin@google.com>
2023-07-26 15:59:48 -0700

    Move connection comment logic to package

--- packages/malloy-malloy-sql/src/malloySQLParser.ts
+++ packages/malloy-malloy-sql/src/malloySQLParser.ts
@@ -156,11 +156,17 @@ export class MalloySQLParser {
           type: MalloySQLStatementType.MALLOY,
         });
       } else {
+        const match = /^--\s*connection:\s*(?<connectionName>\S+)/.exec(
+          parsedStatement.statementText
+        );
+        if (match && match.groups) {
+          config.connection = match.groups['connectionName'];
+        }
         if (!config.connection) {
           if (!previousConnection)
             errors.push(
               this.createParseError(
-                'No connection configuration specified, add "connection: my_connection_name" to this >>>sql line or to an above one',
+                'No connection configuration specified',
                 parsedStatement.delimiterRange,
                 url
               )
