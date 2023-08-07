3bdccc66256ffdd9439bfb944e7d7e15ebef9002
Fernando Arreola <figutierrez@google.com>
2023-08-07 11:11:38 -0700

    Remove quiet.

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --quiet --pure --command "$(cat <<NIXCMD
+nix-shell --pure --command "$(cat <<NIXCMD
   set -euxo pipefail
   export PGHOST=127.0.0.1
   export PGDATABASE=postgres
