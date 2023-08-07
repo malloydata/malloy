d43478bfeb51769797fe166cd4fdba1a990fd362
Fernando Arreola <figutierrez@google.com>
2023-08-07 11:25:35 -0700

    Fix test.

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --pure --command "$(cat <<NIXCMD
+nix-shell --quiet --pure --command "$(cat <<NIXCMD
   set -euxo pipefail
   export PGHOST=127.0.0.1
   export PGDATABASE=postgres
   export PGUSER=673673622326@cloudbuild
   cd /workspace
-  npm ci
-  npm run lint && npm run build && npm run build-duckdb-db && npm run test
+  npm ci --silent
+  npm run lint && npm run build && npm run build-duckdb-db && npm run test-silent
 NIXCMD
 )"
