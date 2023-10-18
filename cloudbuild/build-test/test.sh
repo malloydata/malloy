ad39fff7a37c979ef83e2baacc1f87c72137141b
Will Scullin <willscullin@google.com>
2023-10-18 10:02:09 -0700

    Pass in PG variables rather than hard code (#1414)

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-nix-shell --quiet --pure --command "$(cat <<NIXCMD
+nix-shell --quiet --pure --keep PGHOST --keep PGDATABASE --keep PGUSER --command "$(cat <<NIXCMD
   set -euxo pipefail
-  export PGHOST=127.0.0.1
-  export PGDATABASE=postgres
-  export PGUSER=673673622326@cloudbuild
-  cd /workspace
   npm ci --silent
   npm run lint && npm run build && npm run build-duckdb-db && npm run test-silent
 NIXCMD
