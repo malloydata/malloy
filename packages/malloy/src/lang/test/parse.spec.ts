a18971a309c620f81ff9d544ab1f05553d6721da
Michael Toy <66150587+mtoy-googly-moogly@users.noreply.github.com>
2023-11-30 13:07:49 -0800

    allow "x is null" (#1526)
    * allow "x is null"

--- packages/malloy/src/lang/test/parse.spec.ts
+++ packages/malloy/src/lang/test/parse.spec.ts
@@ -381,7 +381,7 @@ describe('error cascading', () => {
         source: a1 is a extend {
           join_one: b with astr
           dimension:
-            ${'err is null'}
+            ${'err is nothing'}
             ${scalars.map((d, i) => `e${i} is ${d}`).join('\n            ')}
           measure:
             measure_err is count(foo),
@@ -390,10 +390,7 @@ describe('error cascading', () => {
               .join('\n            ')}
         }
       `
-    ).translationToFailWith(
-      "Cannot define 'err', unexpected type: null",
-      "'foo' is not defined"
-    );
+    ).translationToFailWith("'nothing' is not defined", "'foo' is not defined");
   });
 
   test('error type inference is good', () => {
@@ -401,12 +398,12 @@ describe('error cascading', () => {
       const source = `
           source: a1 is a extend {
             dimension:
-              ${'err is null'}
+              ${'err is nothing'}
               dim is length(${scalar}, 1)
           }
         `;
       expect(source).translationToFailWith(
-        "Cannot define 'err', unexpected type: null",
+        "'nothing' is not defined",
         `No matching overload for function length(${typedScalars[scalar]}, number)`
       );
     }
@@ -420,7 +417,7 @@ describe('error cascading', () => {
         }
         run: a1 -> {
           group_by:
-            ${'err is null'}
+            ${'err is nothing'}
           aggregate:
             measure_err is count(foo)
           calculate:
@@ -432,10 +429,7 @@ describe('error cascading', () => {
               .join('\n            ')}
         }
       `
-    ).translationToFailWith(
-      "Cannot define 'err', unexpected type: null",
-      "'foo' is not defined"
-    );
+    ).translationToFailWith("'nothing' is not defined", "'foo' is not defined");
   });
 });
 
