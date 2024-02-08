9cc1a9a6e7122fd2f26fd18178f249210f11e34d
Chandan <cbhagwat@google.com>
2024-02-08 14:56:07 -0800

    Add test

--- test/src/databases/bigquery-postgres/streaming.spec.ts
+++ test/src/databases/bigquery-postgres/streaming.spec.ts
@@ -102,5 +102,53 @@ describe('Streaming tests', () => {
       await csvWriter.process(stream);
       expect(accummulator.accumulatedValue).toBe('code\n1Q9\n');
     });
+
+    it(`stream nested results to CSV - ${databaseName}`, async () => {
+      const stream = runtime
+        .loadModel(
+          `source: airports is ${databaseName}.table('malloytest.airports') extend {
+  rename: facility_type is fac_type
+
+  measure: airport_count is count()
+
+  view: by_state is {
+    where: state != null
+    group_by: state
+    aggregate: airport_count
+    limit: 2
+  }
+
+  view: by_facility_type is {
+    group_by: facility_type
+    aggregate:
+      airport_count
+    limit: 2
+  }
+
+  view: airports_by_region is {
+    group_by: faa_region
+    nest:
+      by_state
+      by_facility_type
+    limit: 2
+    aggregate: airport_count
+  }
+}`
+        )
+        .loadQuery('run: airports -> airports_by_region')
+        .runStream({rowLimit: 1});
+      const accummulator = new StringAccumulator();
+      const csvWriter = new CSVWriter(accummulator);
+      await csvWriter.process(stream);
+      const expectedCsv = `faa_region,by_state,,by_facility_type,,airport_count
+AGL,state,airport_count,facility_type,airport_count,4437
+,IL,890,AIRPORT,3443,
+,OH,749,HELIPORT,826,
+ASW,state,airport_count,facility_type,airport_count,3268
+,TX,1845,AIRPORT,2341,
+,LA,500,HELIPORT,861,
+`;
+      expect(accummulator.accumulatedValue).toBe(expectedCsv);
+    });
   });
 });
