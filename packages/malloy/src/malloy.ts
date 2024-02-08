35c9901e8c11bb0e29735286d64e8446379e1c2d
Chandan <cbhagwat@google.com>
2024-02-07 16:27:42 -0800

    nested csv writer

--- packages/malloy/src/malloy.ts
+++ packages/malloy/src/malloy.ts
@@ -66,6 +66,7 @@ import {
   ImportLocation,
   Annotation,
   NamedModelObject,
+  QueryValue,
 } from './model';
 import {
   Connection,
@@ -3857,6 +3858,26 @@ export class JSONWriter extends DataWriter {
   }
 }
 
+type CellMatrix = {
+  rows: string[];
+  length: number;
+  width: number;
+};
+
+/**
+ * CSV writer class that handles nested data.
+ * This writer creates CSV using a DFS traversal of the result dataset.
+ * Each trivial column value is converted to a CSV of 1x1 matrix and all the
+ * columns are merged together to create a CSV that represents 1 QueryDataRow.
+ * Since this follows DFS, each non trivial data is rendered into a NxM matrix
+ * where N is the number of rows in the nested data and M is the number of
+ * columns it has.
+ * For any row with X number of columns, we end up with X number of NxM matrices
+ * where the value of N,M pair may be different for each column.
+ * We then merge the matrices so that we end up with a larger matrix of size
+ * Max(N)xSum(M) by taking one row of csv from each matric at a time. For any
+ * matrix with N<Max(N), we add a row of empty CSV cells of size N.
+ */
 export class CSVWriter extends DataWriter {
   private readonly columnSeparator = ',';
   private readonly rowSeparator = '\n';
@@ -3882,50 +3903,151 @@ export class CSVWriter extends DataWriter {
     return value;
   }
 
-  private stringify(cell: DataColumn) {
-    if (cell.isNull()) {
+  private stringify(value: QueryValue) {
+    if (value === null) {
       return '';
-    } else if (
-      cell.isArray() ||
-      cell.isRecord() ||
-      cell.isBoolean() ||
-      cell.isNumber()
-    ) {
-      return JSON.stringify(cell.value);
-    } else if (cell.isDate() || cell.isTimestamp()) {
-      return cell.value.toISOString();
-    } else if (cell.isString()) {
-      return cell.value;
+    } else if (value instanceof Date) {
+      return value.toISOString();
+    } else if (typeof value === 'boolean' || typeof value === 'number') {
+      return JSON.stringify(value);
     } else {
-      return `${cell.value}`;
+      return `${value}`;
     }
   }
 
-  async process(data: AsyncIterableIterator<DataRecord>): Promise<void> {
-    let fields;
-    for await (const row of data) {
-      if (fields === undefined) {
-        fields = row.field.allFields;
-        if (this.includeHeader) {
-          for (let fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
-            const field = fields[fieldIndex];
-            this.stream.write(this.escape(field.name));
-            if (fieldIndex !== fields.length - 1) {
-              this.stream.write(this.columnSeparator);
-            }
-          }
-          this.stream.write(this.rowSeparator);
-        }
-      }
-      for (let fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
-        const field = fields[fieldIndex];
-        this.stream.write(this.escape(this.stringify(row.cell(field))));
-        if (fieldIndex !== fields.length - 1) {
-          this.stream.write(this.columnSeparator);
-        }
-      }
-      this.stream.write(this.rowSeparator);
+  // Extra weight to be added becase of nested tables inside the cells.
+  private getColWeight(jsonVal: QueryDataRow | QueryData) {
+    let firstVal = jsonVal;
+    if (Array.isArray(jsonVal)) {
+      firstVal = jsonVal[0];
     }
-    this.stream.close();
+    let numKeys = 0;
+    for (const key in firstVal) {
+      numKeys = numKeys + 1;
+      const val = firstVal[key];
+      if (Array.isArray(val)) {
+        const weight = this.getColWeight(val) - 1;
+        numKeys = numKeys + weight;
+      }
+    }
+    return numKeys;
+  }
+
+  // Get header row along with extra empty spaces for nested children.
+  private getHeaderRow(row: QueryDataRow): CellMatrix {
+    let csv = '';
+    let width = 0;
+    for (const key in row) {
+      csv = csv + this.escape(key) + ',';
+      const val = row[key];
+      width++;
+      if (Array.isArray(val)) {
+        const numKeys = this.getColWeight(val) - 1;
+        width = width + numKeys;
+        for (let i = 0; i < numKeys; i++) {
+          csv = csv + ',';
+        }
+      }
+    }
+    return {rows: [csv], length: 1, width: width};
+  }
+
+  // Merge the child matrices i.e. merge the columns into one bigger matrix i.e. CSV.
+  private mergeMatrices(matrices: CellMatrix[]): CellMatrix {
+    const maxLength = Math.max(...matrices.map(matrix => matrix.length));
+    const matrixWidth = matrices.reduce((sum, matrix) => sum + matrix.width, 0);
+    const csvMatrix: string[] = [];
+    for (let i = 0; i < maxLength; i++) {
+      let csvRow = '';
+      for (const matrix of matrices) {
+        if (i < matrix.length) {
+          csvRow = csvRow + matrix.rows[i];
+          if (!csvRow.endsWith(',')) {
+            csvRow = csvRow + ',';
+          }
+        } else {
+          // Add empty cells.
+          for (let w = 0; w < matrix.width; w++) {
+            csvRow = csvRow + ',';
+          }
+        }
+      }
+      csvMatrix.push(csvRow);
+    }
+    return {
+      rows: csvMatrix,
+      length: maxLength,
+      width: matrixWidth,
+    };
+  }
+
+  // Gets CSV for a data cell that has nested data.
+  private getChildMatrix(jsonVal: QueryData): CellMatrix {
+    // This is not expected to happen.
+    if (!Array.isArray(jsonVal)) {
+      return {
+        rows: ['Invalid data found, value is not an array'],
+        length: 1,
+        width: 1,
+      };
+    } else if (jsonVal.length === 0) {
+      return {
+        rows: [''],
+        length: 1,
+        width: 1,
+      };
+    }
+    const csvMatrix: string[] = [];
+
+    const header = this.getHeaderRow(jsonVal[0]);
+    // Header has 1 row.
+    csvMatrix.push(...header.rows);
+    const width = header.width;
+    let rowCount = 1;
+
+    for (const row of jsonVal) {
+      const rowMatrix = this.getRowMatrix(row);
+      rowCount = rowCount + rowMatrix.length;
+      csvMatrix.push(...rowMatrix.rows);
+    }
+
+    return {rows: csvMatrix, length: rowCount, width: width};
+  }
+
+  // Creates CSV content for one row of data.
+  private getRowMatrix(row: QueryDataRow) {
+    const matrices: CellMatrix[] = [];
+    for (const key in row) {
+      const val = row[key];
+      if (!Array.isArray(val)) {
+        const cell = {
+          rows: [this.stringify(val)],
+          length: 1,
+          width: 1,
+        };
+        matrices.push(cell);
+      } else {
+        const cell = this.getChildMatrix(val);
+        matrices.push(cell);
+      }
+    }
+    return this.mergeMatrices(matrices);
+  }
+
+  async process(data: AsyncIterableIterator<DataRecord>): Promise<void> {
+    let headerDefined = false;
+    for await (const row of data) {
+      if (!headerDefined && this.includeHeader) {
+        const header: CellMatrix = this.getHeaderRow(row.toObject());
+        this.stream.write(header.rows[0]);
+        this.stream.write(this.rowSeparator);
+        headerDefined = true;
+      }
+      const rowCsv = this.getRowMatrix(row.toObject());
+      for (const line of rowCsv.rows) {
+        this.stream.write(line);
+        this.stream.write(this.rowSeparator);
+      }
+    }
   }
 }
