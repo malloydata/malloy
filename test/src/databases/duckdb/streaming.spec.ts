1b30417b5b48d851a4b31e1f70705164bef1ef26
Chandan <cbhagwat@google.com>
2024-02-08 16:08:43 -0800

    add to duckdb test  instead

--- test/src/databases/duckdb/streaming.spec.ts
+++ test/src/databases/duckdb/streaming.spec.ts
@@ -21,14 +21,9 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import {
-  CSVWriter,
-  DataRecord,
-  JSONWriter,
-  WriteStream,
-} from '@malloydata/malloy';
-import {RuntimeList} from '../../runtimes';
+import {WriteStream, CSVWriter} from '@malloydata/malloy';
 import {describeIfDatabaseAvailable} from '../../util';
+import {RuntimeList} from '../../runtimes';
 
 class StringAccumulator implements WriteStream {
   public accumulatedValue = '';
@@ -42,10 +37,9 @@ class StringAccumulator implements WriteStream {
   }
 }
 
-const [describe, databases] = describeIfDatabaseAvailable([
-  'bigquery',
-  'postgres',
-]);
+const runtimes = ['duckdb'];
+
+const [_describe, databases] = describeIfDatabaseAvailable(runtimes);
 
 describe('Streaming tests', () => {
   const runtimes = new RuntimeList(databases);
@@ -55,52 +49,52 @@ describe('Streaming tests', () => {
   });
 
   runtimes.runtimeMap.forEach((runtime, databaseName) => {
-    it(`basic stream test  - ${databaseName}`, async () => {
+    it(`stream nested results to CSV - ${databaseName}`, async () => {
       const stream = runtime
         .loadModel(
-          `source: airports is ${databaseName}.table('malloytest.airports')`
-        )
-        .loadQuery('run: airports -> { select: code }')
-        .runStream({rowLimit: 10});
-      const rows: DataRecord[] = [];
-      for await (const row of stream) {
-        rows.push(row);
-      }
-      expect(rows.length).toBe(10);
-      expect(rows[0].cell('code').string.value).toBe('1Q9');
-    });
+          `source: airports is ${databaseName}.table('test/data/duckdb/airports.parquet') extend {
+  rename: facility_type is fac_type
 
-    it(`stream to JSON - ${databaseName}`, async () => {
-      const stream = runtime
-        .loadModel(
-          `source: airports is ${databaseName}.table('malloytest.airports')`
-        )
-        .loadQuery('run: airports -> { select: code }')
-        .runStream({rowLimit: 1});
-      const accummulator = new StringAccumulator();
-      const jsonWriter = new JSONWriter(accummulator);
-      await jsonWriter.process(stream);
-      expect(accummulator.accumulatedValue).toBe(
-        `[
-  {
-    "code": "1Q9"
+  measure: airport_count is count()
+
+  view: by_state is {
+    where: state != null
+    group_by: state
+    aggregate: airport_count
+    limit: 2
   }
-]
-`
-      );
-    });
 
-    it(`stream to CSV - ${databaseName}`, async () => {
-      const stream = runtime
-        .loadModel(
-          `source: airports is ${databaseName}.table('malloytest.airports')`
+  view: by_facility_type is {
+    group_by: facility_type
+    aggregate:
+      airport_count
+    limit: 2
+  }
+
+  view: airports_by_region is {
+    group_by: faa_region
+    nest:
+      by_state
+      by_facility_type
+    limit: 2
+    aggregate: airport_count
+  }
+}`
         )
-        .loadQuery('run: airports -> { select: code }')
-        .runStream({rowLimit: 1});
+        .loadQuery('run: airports -> airports_by_region')
+        .runStream();
       const accummulator = new StringAccumulator();
       const csvWriter = new CSVWriter(accummulator);
       await csvWriter.process(stream);
-      expect(accummulator.accumulatedValue).toBe('code\n1Q9\n');
+      const expectedCsv = `faa_region,by_state,,by_facility_type,,airport_count
+AGL,state,airport_count,facility_type,airport_count,4437
+,IL,890,AIRPORT,3443,
+,OH,749,HELIPORT,826,
+ASW,state,airport_count,facility_type,airport_count,3268
+,TX,1845,AIRPORT,2341,
+,LA,500,HELIPORT,861,
+`;
+      expect(accummulator.accumulatedValue).toBe(expectedCsv);
     });
   });
 });
