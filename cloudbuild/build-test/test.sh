269fad7c54fa781e36553576f9284b6eb90104ed
Will Scullin <willscullin@google.com>
2024-03-14 09:04:02 -0700

    Postgres action (#1659)
    Postgres action

--- cloudbuild/build-test/test.sh
+++ cloudbuild/build-test/test.sh
@@ -10,10 +10,12 @@ nix-shell \
   --command "$(cat <<NIXCMD
   set -euxo pipefail
   npm ci --loglevel error
-  export MALLOY_DATABASES=postgres,bigquery,duckdb,duckdb_wasm
+  export MALLOY_DATABASES=bigquery,duckdb,duckdb_wasm
   npm run lint
   npm run build
   npm run build-duckdb-db
   npm run test-silent
+  export MALLOY_DATABASES=bigquery,postgres
+  npm run test-silent -- -- test/src/databases/bigquery-postgres/multi_connection.spec.ts
 NIXCMD
 )"
