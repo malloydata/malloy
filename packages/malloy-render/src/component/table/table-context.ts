64e5dd9e0a361794c0d7af8c20f06706db7e331f
Speros Kokenes <speros@meta.com>
2024-08-19 11:11:04 -0400

    table virtualization poc
    feat: virtualize

--- packages/malloy-render/src/component/table/table-context.ts
+++ packages/malloy-render/src/component/table/table-context.ts
@@ -1,13 +1,25 @@
 import {createContext, useContext} from 'solid-js';
-import {createStore} from 'solid-js/store';
+import {createStore, SetStoreFunction, Store} from 'solid-js/store';
 import {TableLayout} from './table-layout';
+
+type TableStore = {
+  headerSizes: Record<string, number>;
+  columnWidths: Record<string, number>;
+};
 
 type TableContext = {
   root: boolean;
-  pinnedHeader: boolean;
   layout: TableLayout;
+  store: Store<TableStore>;
+  setStore: SetStoreFunction<TableStore>;
   headerSizeStore: ReturnType<typeof createStore<Record<string, number>>>;
 };
 
 export const TableContext = createContext<TableContext>();
 export const useTableContext = () => useContext(TableContext);
+export function createTableStore() {
+  return createStore<TableStore>({
+    headerSizes: {},
+    columnWidths: {},
+  });
+}
