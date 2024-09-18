d8195bdd4537cdda1a10cdfdeccaf44cf41ebcad
Will Scullin <scullin@meta.com>
2024-09-18 14:40:53 -0700

    Update expected value for new database

--- test/src/databases/streaming/streaming.spec.ts
+++ test/src/databases/streaming/streaming.spec.ts
@@ -72,7 +72,7 @@ describe('Streaming tests', () => {
         rows.push(row);
       }
       expect(rows.length).toBe(10);
-      expect(rows[0].cell('code').string.value).toBe('1Q9');
+      expect(rows[0].cell('code').string.value).toBe('6A8');
     });
 
     it(`stream to JSON - ${databaseName}`, async () => {
@@ -88,7 +88,7 @@ describe('Streaming tests', () => {
       expect(accummulator.accumulatedValue).toBe(
         `[
   {
-    "code": "1Q9"
+    "code": "6A8"
   }
 ]
 `
@@ -105,7 +105,7 @@ describe('Streaming tests', () => {
       const accummulator = new StringAccumulator();
       const csvWriter = new CSVWriter(accummulator);
       await csvWriter.process(stream);
-      expect(accummulator.accumulatedValue).toBe('code\n1Q9\n');
+      expect(accummulator.accumulatedValue).toBe('code\n6A8\n');
     });
   });
 });
