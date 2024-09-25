cc0e59622362fbadcad554246cdfd9f4bd26408d
Speros Kokenes <speros@meta.com>
2024-09-25 15:40:46 -0400

    fix: tooltip style scoping

--- packages/malloy-render/src/component/register-webcomponent.ts
+++ packages/malloy-render/src/component/register-webcomponent.ts
@@ -4,7 +4,6 @@ import {MalloyRender, MalloyRenderProps} from './render';
 import css from './render.css?raw';
 import tableCss from './table/table.css?raw';
 import dashboardCss from './dashboard/dashboard.css?raw';
-import tooltipCss from './tooltip/tooltip.css?raw';
 
 const withStyles = ComponentType => {
   return (props, options) => {
@@ -12,9 +11,7 @@ const withStyles = ComponentType => {
     const stylesheet = new CSSStyleSheet();
     stylesheet.replaceSync(css);
     const tableStylesheet = new CSSStyleSheet();
-    tableStylesheet.replaceSync(
-      [tableCss, dashboardCss, tooltipCss].join('\n')
-    );
+    tableStylesheet.replaceSync([tableCss, dashboardCss].join('\n'));
     element.renderRoot.adoptedStyleSheets = [stylesheet, tableStylesheet];
     return ComponentType(props, options);
   };
