de104553107f79a687c01317bdcd433ce4539120
Will Scullin <scullin@meta.com>
2024-10-07 09:18:18 -0700

    Init required extension

--- test/bin/postgres_init.sh
+++ test/bin/postgres_init.sh
@@ -7,3 +7,4 @@ initdb -d .tmp/data/malloytestdb --no-locale --encoding=UTF8
 pg_ctl -D .tmp/data/malloytestdb -l .tmp/logfile -o "--unix_socket_directories='$PWD/.tmp'" start
 createdb $USER
 gunzip -c ../data/postgres/malloytest-postgres.sql.gz | psql
+echo CREATE EXTENSION tsm_system_rows\; | psql
