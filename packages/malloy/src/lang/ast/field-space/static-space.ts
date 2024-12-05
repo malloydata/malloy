f6b17b12930576d5c6bbf45eb6e98008adc03a55
Michael Toy <66150587+mtoy-googly-moogly@users.noreply.github.com>
2024-12-05 15:08:59 -0800

    all hour our benevolent protectors

--- packages/malloy/src/lang/ast/field-space/static-space.ts
+++ packages/malloy/src/lang/ast/field-space/static-space.ts
@@ -151,10 +151,7 @@ export class StaticSpace implements FieldSpace {
     if (found instanceof SpaceField) {
       const definition = found.fieldDef();
       if (definition) {
-        if (
-          !(found instanceof StructSpaceFieldBase) &&
-          isJoined(definition)
-        ) {
+        if (!(found instanceof StructSpaceFieldBase) && isJoined(definition)) {
           // We have looked up a field which is a join, but not a StructSpaceField
           // because it is someting like "dimension: joinedArray is arrayComputation"
           // which wasn't known to be a join when the fieldspace was constructed.
