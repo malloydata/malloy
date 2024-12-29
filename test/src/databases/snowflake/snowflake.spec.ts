3ce2e24f6676ca8e779b4c2edaab47bc439a96ce
Jason Smart <jlsmart@meta.com>
2024-12-29 14:38:19 -0800

    Added a custom HyperLogLog test for Snowflake.  This test is similar to BigQuery, but the two estimators return different values, reflecting the probabalistic nature of the algorithm.  Thus we provide standalone tests for each.
    Signed-off-by: Jason Smart <jlsmart@meta.com>

---
+++ test/src/databases/snowflake/snowflake.spec.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint-disable no-console */
+
+import {RuntimeList} from '../../runtimes';
+import {describeIfDatabaseAvailable} from '../../util';
+import '../../util/db-jest-matchers';
+
+const [describe, databases] = describeIfDatabaseAvailable(['snowflake']);
+const runtimes = new RuntimeList(databases);
+
+describe.each(runtimes.runtimeList)(
+  'Snowflake dialect functions - %s',
+
+  (databaseName, runtime) => {
+    if (runtime === undefined) {
+      throw new Error("Couldn't build runtime");
+    }
+    it(`hyperloglog combine`, async () => {
+      await expect(`run: bigquery.table('malloytest.airports')->{
+          aggregate: code_hll is hll_accumulate(code)
+      } -> {
+          aggregate: code_count is hll_estimate(hll_combine(code_hll))
+      }
+      `).malloyResultMatches(runtime, {code_count: 19812});
+    });
+  }
+);
