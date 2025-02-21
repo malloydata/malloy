925c1c03398693621bd71b059d6fd134e30fffc9
Jason Smart <jlsmart@meta.com>
2025-02-21 09:14:10 -0800

    Updates to get CI working correctly.
    Signed-off-by: Jason Smart <jlsmart@meta.com>

--- jest.core.config.ts
+++ jest.core.config.ts
@@ -1,9 +1,10 @@
 module.exports = {
   ...require('./jest.config.ts'),
   roots: [
+    '<rootDir>/packages/malloy/',
+    '<rootDir>/packages/malloy-filter/',
     '<rootDir>/packages/malloy-malloy-sql/',
     '<rootDir>/packages/malloy-syntax-highlight/',
-    '<rootDir>/packages/malloy/',
     '<rootDir>/test/src/core/',
     '<rootDir>/test/src/render/',
   ],
--- packages/malloy-filter/jest.config.js
+++ packages/malloy-filter/jest.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  collectCoverage: true,
-  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  moduleFileExtensions: ['js'],
 };
