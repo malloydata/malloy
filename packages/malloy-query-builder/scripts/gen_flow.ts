44f65b88b2b9e895ecd1a8dffa71b7eb1124ac20
Christopher Swenson <cswenson@meta.com>
2025-02-23 16:07:16 -0600

    For now remove the flow test

--- packages/malloy-query-builder/scripts/gen_flow.ts
+++ packages/malloy-query-builder/scripts/gen_flow.ts
@@ -3,9 +3,17 @@ import {unstable_translateTSDefToFlowDef} from 'flow-api-translator';
 import * as fs from 'fs';
 
 async function go() {
-  const files = ['index.d.ts', 'query-ast.d.ts'];
+  const skipFiles = [
+    'expects.d.ts',
+    'query-ast.spec.d.ts',
+    'query-ast.spec.js.map',
+    'query-ast.spec.js',
+    'query-ast.js.map',
+  ];
+  const files = fs.readdirSync('./dist').filter(f => !skipFiles.includes(f));
   await Promise.all(
     files.map(async file => {
+      console.log(`Generating flow types for file ${file}`);
       const contents = fs.readFileSync(`./dist/${file}`, 'utf8');
       const flow = await unstable_translateTSDefToFlowDef(contents);
       console.log(flow);
