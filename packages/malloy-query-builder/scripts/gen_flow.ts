314bcae4eea7e9316db69ae869a3c208faa21046
Christopher Swenson <cswenson@meta.com>
2025-02-24 09:35:53 -0600

    Make flow script write to file rather than console.log

--- packages/malloy-query-builder/scripts/gen_flow.ts
+++ packages/malloy-query-builder/scripts/gen_flow.ts
@@ -7,14 +7,15 @@ async function go() {
   const files = fs
     .readdirSync('./dist')
     .filter(f => f.endsWith('.d.ts') && !skipFiles.includes(f));
+  if (fs.existsSync('./flow')) fs.rmdirSync('./flow', {recursive: true});
+  fs.mkdirSync('./flow');
   await Promise.all(
     files.map(async file => {
       // eslint-disable-next-line no-console
       console.log(`Generating flow types for file ${file}`);
       const contents = fs.readFileSync(`./dist/${file}`, 'utf8');
       const flow = await unstable_translateTSDefToFlowDef(contents);
-      // eslint-disable-next-line no-console
-      console.log(flow);
+      await fs.promises.writeFile(`./flow/${file}`, flow);
     })
   );
 }
