5892a6f0686498442a727b243053ded8bfd2ed94
Michael Toy <66150587+mtoy-googly-moogly@users.noreply.github.com>
2025-03-02 10:10:49 -0800

    add meta copyrights

--- packages/malloy-filter/src/fstring.spec.ts
+++ packages/malloy-filter/src/fstring.spec.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {diff} from 'jest-diff';
 import * as nearley from 'nearley';
 import fstring_grammar from './lib/fexpr-string-parser';
