b1535f530b031bf003fbeffa21f75193e5efe6d3
Chris Swenson <cswenson@meta.com>
2025-03-05 10:15:30 -0600

    Pull in changes to `modelDefToModelInfo` from https://github.com/malloydata/malloy/pull/2172 (#2176)
    * Pull in changes to `modelDefToModelInfo` from https://github.com/malloydata/malloy/pull/2172
    
    * Remove commented code

--- packages/malloy/src/to_stable.ts
+++ packages/malloy/src/to_stable.ts
@@ -13,8 +13,11 @@ import {
   isTurtle,
   JoinType,
   ModelDef,
+  Query,
   RecordTypeDef,
   RepeatedRecordTypeDef,
+  ResultMetadataDef,
+  ResultStructMetadataDef,
   SourceDef,
   TimestampUnit,
 } from './model';
@@ -22,6 +25,8 @@ import {
   getResultStructDefForQuery,
   getResultStructDefForView,
 } from './model/malloy_query';
+import {annotationToTaglines} from './annotation';
+import {Tag} from '@malloydata/malloy-tag';
 
 export function modelDefToModelInfo(modelDef: ModelDef): Malloy.ModelInfo {
   const modelInfo: Malloy.ModelInfo = {
@@ -41,36 +46,83 @@ export function modelDefToModelInfo(modelDef: ModelDef): Malloy.ModelInfo {
       modelInfo.entries.push(sourceInfo);
     } else if (entry.type === 'query') {
       const outputStruct = getResultStructDefForQuery(modelDef, entry);
+      const annotations = getAnnotationsFromField(entry);
+      const resultMetadataAnnotation = outputStruct.resultMetadata
+        ? getResultStructMetadataAnnotation(
+            outputStruct,
+            outputStruct.resultMetadata
+          )
+        : undefined;
+      const fieldAnnotations = [
+        ...(annotations ?? []),
+        ...(resultMetadataAnnotation ? [resultMetadataAnnotation] : []),
+      ];
       const queryInfo: Malloy.ModelEntryValueWithSource = {
         kind: 'source',
         name,
         schema: {
           fields: convertFieldInfos(outputStruct, outputStruct.fields),
         },
+        annotations: fieldAnnotations.length > 0 ? fieldAnnotations : undefined,
       };
       modelInfo.entries.push(queryInfo);
     }
   }
   for (const query of modelDef.queryList) {
     const outputStruct = getResultStructDefForQuery(modelDef, query);
+    const annotations = getAnnotationsFromField(query);
+    const resultMetadataAnnotation = outputStruct.resultMetadata
+      ? getResultStructMetadataAnnotation(
+          outputStruct,
+          outputStruct.resultMetadata
+        )
+      : undefined;
+    const fieldAnnotations = [
+      ...(annotations ?? []),
+      ...(resultMetadataAnnotation ? [resultMetadataAnnotation] : []),
+    ];
     const queryInfo: Malloy.AnonymousQueryInfo = {
       schema: {
         fields: convertFieldInfos(outputStruct, outputStruct.fields),
       },
+      annotations: fieldAnnotations.length > 0 ? fieldAnnotations : undefined,
     };
     modelInfo.anonymous_queries.push(queryInfo);
   }
   return modelInfo;
 }
 
-function convertFieldInfos(source: SourceDef, fields: FieldDef[]) {
+function getAnnotationsFromField(field: FieldDef | Query): Malloy.Annotation[] {
+  const taglines = annotationToTaglines(field.annotation);
+  return taglines.map(tagline => ({
+    value: tagline,
+  }));
+}
+
+export function convertFieldInfos(source: SourceDef, fields: FieldDef[]) {
   const result: Malloy.FieldInfo[] = [];
   for (const field of fields) {
+    const taglines = annotationToTaglines(field.annotation);
+    const rawAnnotations: Malloy.Annotation[] = taglines.map(tagline => ({
+      value: tagline,
+    }));
+    const annotations = rawAnnotations.length > 0 ? rawAnnotations : undefined;
     if (isTurtle(field)) {
       const outputStruct = getResultStructDefForView(source, field);
+      const resultMetadataAnnotation = outputStruct.resultMetadata
+        ? getResultStructMetadataAnnotation(
+            outputStruct,
+            outputStruct.resultMetadata
+          )
+        : undefined;
+      const fieldAnnotations = [
+        ...(annotations ?? []),
+        ...(resultMetadataAnnotation ? [resultMetadataAnnotation] : []),
+      ];
       const fieldInfo: Malloy.FieldInfo = {
         kind: 'view',
         name: field.as ?? field.name,
+        annotations: fieldAnnotations.length > 0 ? fieldAnnotations : undefined,
         schema: {fields: convertFieldInfos(outputStruct, outputStruct.fields)},
       };
       result.push(fieldInfo);
@@ -79,16 +131,25 @@ function convertFieldInfos(source: SourceDef, fields: FieldDef[]) {
       const scalar = expressionIsScalar(field.expressionType);
       if (!aggregate && !scalar) continue;
       if (field.type === 'error') continue;
+      const resultMetadataAnnotation = field.resultMetadata
+        ? getResultMetadataAnnotation(field, field.resultMetadata)
+        : undefined;
+      const fieldAnnotations = [
+        ...(annotations ?? []),
+        ...(resultMetadataAnnotation ? [resultMetadataAnnotation] : []),
+      ];
       const fieldInfo: Malloy.FieldInfo = {
         kind: aggregate ? 'measure' : 'dimension',
         name: field.as ?? field.name,
         type: typeDefToType(field),
+        annotations: fieldAnnotations.length > 0 ? fieldAnnotations : undefined,
       };
       result.push(fieldInfo);
     } else if (isJoinedSource(field)) {
       const fieldInfo: Malloy.FieldInfo = {
         kind: 'join',
         name: field.as ?? field.name,
+        annotations,
         schema: {
           fields: convertFieldInfos(field, field.fields),
         },
@@ -98,6 +159,68 @@ function convertFieldInfos(source: SourceDef, fields: FieldDef[]) {
     }
   }
   return result;
+}
+
+function getResultMetadataAnnotation(
+  field: FieldDef,
+  resultMetadata: ResultMetadataDef
+): Malloy.Annotation | undefined {
+  const tag = Tag.withPrefix('#(malloy) ');
+  let hasAny = false;
+  if (resultMetadata.referenceId !== undefined) {
+    tag.set(['reference_id'], resultMetadata.referenceId);
+    hasAny = true;
+  }
+  if (resultMetadata.fieldKind === 'measure') {
+    tag.set(['calculation']);
+    hasAny = true;
+  }
+  if (resultMetadata.fieldKind === 'dimension') {
+    const dot = '.';
+    // If field is joined-in from another table i.e. of type `tableName.columnName`,
+    // return sourceField, else return name because this could be a renamed field.
+    const drillExpression =
+      resultMetadata?.sourceExpression ||
+      (resultMetadata?.sourceField.includes(dot)
+        ? resultMetadata?.sourceField
+        : field.name);
+    tag.set(['drill_expression'], drillExpression);
+    hasAny = true;
+  }
+  return hasAny
+    ? {
+        value: tag.toString(),
+      }
+    : undefined;
+}
+
+function getResultStructMetadataAnnotation(
+  field: SourceDef,
+  resultMetadata: ResultStructMetadataDef
+): Malloy.Annotation | undefined {
+  const tag = Tag.withPrefix('#(malloy) ');
+  let hasAny = false;
+  if (resultMetadata.limit !== undefined) {
+    tag.set(['limit'], resultMetadata.limit);
+    hasAny = true;
+  }
+  if (resultMetadata.orderBy) {
+    for (let i = 0; i < resultMetadata.orderBy.length; i++) {
+      const orderBy = resultMetadata.orderBy[i];
+      const orderByField =
+        typeof orderBy.field === 'number'
+          ? field.fields[orderBy.field].as ?? field.fields[orderBy.field].name
+          : orderBy.field;
+      const direction = orderBy.dir ?? null;
+      tag.set(['ordered_by', i, orderByField], direction);
+    }
+    hasAny = true;
+  }
+  return hasAny
+    ? {
+        value: tag.toString(),
+      }
+    : undefined;
 }
 
 function typeDefToType(field: AtomicTypeDef): Malloy.AtomicType {
@@ -117,11 +240,21 @@ function typeDefToType(field: AtomicTypeDef): Malloy.AtomicType {
         };
       case 'boolean':
         return {kind: 'boolean_type'};
-      case 'date':
+      case 'date': {
+        // TODO there seems to be a bug where date literals with a timestamp truncation have
+        // type: date, but still have a timestamp truncation.
+        const timeframe = field.timeframe;
+        if (timeframe && !isDateTimeframe(timeframe)) {
+          return {
+            kind: 'timestamp_type',
+            timeframe: convertTimestampTimeframe(field.timeframe),
+          };
+        }
         return {
           kind: 'date_type',
           timeframe: convertDateTimeframe(field.timeframe),
         };
+      }
       case 'timestamp':
         return {
           kind: 'timestamp_type',
@@ -159,9 +292,27 @@ function convertRecordType(
   return {
     kind: 'record_type',
     fields: field.fields.map(f => {
+      const annotations: Malloy.Annotation[] = [];
+      if ('resultMetadata' in f) {
+        if (f.resultMetadata) {
+          const ann = getResultMetadataAnnotation(f, f.resultMetadata);
+          if (ann) {
+            annotations.push(ann);
+          }
+        }
+      }
+      if (f.annotation) {
+        const taglines = annotationToTaglines(f.annotation);
+        annotations.push(
+          ...taglines.map(tagline => ({
+            value: tagline,
+          }))
+        );
+      }
       if (isAtomic(f)) {
         return {
           name: f.name,
+          annotations: annotations.length > 0 ? annotations : undefined,
           type: typeDefToType(f),
         };
       } else {
@@ -171,6 +322,19 @@ function convertRecordType(
       }
     }),
   };
+}
+
+function isDateTimeframe(timeframe: DateUnit): boolean {
+  switch (timeframe) {
+    case 'day':
+    case 'week':
+    case 'month':
+    case 'year':
+    case 'quarter':
+      return true;
+    default:
+      return false;
+  }
 }
 
 function convertDateTimeframe(
@@ -186,7 +350,7 @@ function convertDateTimeframe(
     case 'quarter':
       return timeframe;
     default:
-      throw new Error('Invalid date timeframe');
+      throw new Error(`Invalid date timeframe ${timeframe}`);
   }
 }
 
