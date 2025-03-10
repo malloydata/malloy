ec92fd099fa3da7364907c4629f849d01435c3d8
Will Scullin <scullin@meta.com>
2025-03-10 12:49:30 -0700

    More packages

---
+++ packages/malloy-filter/scripts/gen_flow.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line node/no-unpublished-import
+import {unstable_translateTSDefToFlowDef} from 'flow-api-translator';
+import * as fs from 'fs';
+
+async function go() {
+  const files = fs
+    .readdirSync('./dist')
+    .filter(f => f.endsWith('.d.ts') && !f.endsWith('.spec.d.ts'));
+  if (fs.existsSync('./@flowtyped'))
+    fs.rmdirSync('./@flowtyped', {recursive: true});
+  fs.mkdirSync('./@flowtyped');
+  await Promise.all(
+    files.map(async file => {
+      // eslint-disable-next-line no-console
+      console.log(`Generating flow types for file ${file}`);
+      const contents = fs.readFileSync(`./dist/${file}`, 'utf8');
+      const flow = await unstable_translateTSDefToFlowDef(contents);
+      await fs.promises.writeFile(
+        `./@flowtyped/${file.replace('.d.ts', '.js.flow')}`,
+        '// @flow\n' + flow
+      );
+    })
+  );
+}
+
+go();
