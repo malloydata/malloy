4e67dad627dba62bfd20fe5e437bc0b2f63386cb
vitorelourenco <vitor.registros@lourencos.net>
2025-03-24 11:37:52 -0300

    Write basic sqlserver connection class

---
+++ packages/malloy-db-sqlserver/src/sqlserver.spec.ts
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import {SqlServerConnection} from './sqlserver_connection';
+import type {SQLSourceDef} from '@malloydata/malloy';
+import {describeIfDatabaseAvailable} from '@malloydata/malloy/test';
+
+const [describe] = describeIfDatabaseAvailable(['sqlserver']);
+
+/*
+ * !IMPORTANT
+ *
+ * The connection is reused for each test, so if you do not name your tables
+ * and keys uniquely for each test you will see cross test interactions.
+ */
+
+describe('SqlServerConnection', () => {
+  let connection: SqlServerConnection;
+  let getTableSchema: jest.SpyInstance;
+  let getSQLBlockSchema: jest.SpyInstance;
+
+  beforeAll(async () => {
+    connection = new SqlServerConnection('duckdb');
+    await connection.runSQL('SELECT 1');
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  beforeEach(async () => {
+    getTableSchema = jest
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .spyOn(SqlServerConnection.prototype as any, 'fetchTableSchema')
+      .mockResolvedValue({
+        type: 'table',
+        dialect: 'SqlServer',
+        name: 'name',
+        tablePath: 'test',
+        connection: 'SqlServer',
+      });
+
+    getSQLBlockSchema = jest
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .spyOn(SqlServerConnection.prototype as any, 'fetchSelectSchema')
+      .mockResolvedValue({
+        type: 'sql select',
+        dialect: 'SqlServer',
+        name: 'name',
+        selectStr: SQL_BLOCK_1.selectStr,
+        connection: 'SqlServer',
+        fields: [],
+      });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('caches table schema', async () => {
+    await connection.fetchSchemaForTables({'test1': 'table1'}, {});
+    expect(getTableSchema).toBeCalledTimes(1);
+    await connection.fetchSchemaForTables({'test1': 'table1'}, {});
+    expect(getTableSchema).toBeCalledTimes(1);
+  });
+
+  it('refreshes table schema', async () => {
+    await connection.fetchSchemaForTables({'test2': 'table2'}, {});
+    expect(getTableSchema).toBeCalledTimes(1);
+    await connection.fetchSchemaForTables(
+      {'test2': 'table2'},
+      {refreshTimestamp: Date.now() + 10}
+    );
+    expect(getTableSchema).toBeCalledTimes(2);
+  });
+
+  it('caches sql schema', async () => {
+    await connection.fetchSchemaForSQLStruct(SQL_BLOCK_1, {});
+    expect(getSQLBlockSchema).toBeCalledTimes(1);
+    await connection.fetchSchemaForSQLStruct(SQL_BLOCK_1, {});
+    expect(getSQLBlockSchema).toBeCalledTimes(1);
+  });
+
+  it('refreshes sql schema', async () => {
+    await connection.fetchSchemaForSQLStruct(SQL_BLOCK_2, {});
+    expect(getSQLBlockSchema).toBeCalledTimes(1);
+    await connection.fetchSchemaForSQLStruct(SQL_BLOCK_2, {
+      refreshTimestamp: Date.now() + 10,
+    });
+    expect(getSQLBlockSchema).toBeCalledTimes(2);
+  });
+});
+
+const SQL_BLOCK_1: SQLSourceDef = {
+  type: 'sql_select',
+  name: 'block1',
+  dialect: 'SqlServer',
+  connection: 'SqlServer',
+  fields: [],
+  selectStr: `
+SELECT
+created_at,
+sale_price,
+inventory_item_id
+FROM 'order_items.parquet'
+SELECT
+id,
+product_department,
+product_category,
+created_at AS inventory_items_created_at
+FROM "inventory_items.parquet"
+`,
+};
+
+const SQL_BLOCK_2: SQLSourceDef = {
+  type: 'sql_select',
+  name: 'block2',
+  dialect: 'SqlServer',
+  connection: 'SqlServer',
+  fields: [],
+  selectStr: `
+SELECT
+created_at,
+sale_price,
+inventory_item_id
+FROM read_parquet('order_items2.parquet', arg='value')
+SELECT
+id,
+product_department,
+product_category,
+created_at AS inventory_items_created_at
+FROM read_parquet("inventory_items2.parquet")
+`,
+};
---
+++ packages/malloy-db-sqlserver/src/sqlserver_connection.ts
@@ -0,0 +1,459 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// LTNOTE: we need this extension to be installed to correctly index
+//  SqlServer data...  We should probably do this on connection creation...
+//
+//     create extension if not exists tsm_system_rows
+//
+
+import * as crypto from 'crypto';
+import type {
+  Connection,
+  ConnectionConfig,
+  MalloyQueryData,
+  PersistSQLResults,
+  PooledConnection,
+  QueryData,
+  QueryDataRow,
+  QueryOptionsReader,
+  QueryRunStats,
+  RunSQLOptions,
+  SQLSourceDef,
+  TableSourceDef,
+  StreamingConnection,
+  StructDef,
+  SQLSourceRequest,
+} from '@malloydata/malloy';
+import {SqlServerDialect, mkArrayDef, sqlKey} from '@malloydata/malloy';
+import {BaseConnection} from '@malloydata/malloy/connection';
+
+import {connect, ConnectionPool} from 'mssql';
+import {randomUUID} from 'crypto';
+
+interface SqlServerConnectionConfiguration {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  databaseName?: string;
+}
+
+type SqlServerConnectionConfigurationReader =
+  | SqlServerConnectionConfiguration
+  | (() => Promise<SqlServerConnectionConfiguration>);
+
+const DEFAULT_PAGE_SIZE = 1000;
+const SCHEMA_PAGE_SIZE = 1000;
+
+export interface SqlServerConnectionOptions
+  extends ConnectionConfig,
+    SqlServerConnectionConfiguration {}
+
+export class SqlServerConnection
+  extends BaseConnection
+  implements Connection, StreamingConnection, PersistSQLResults
+{
+  public readonly name: string;
+  private queryOptionsReader: QueryOptionsReader = {};
+  private configReader: SqlServerConnectionConfigurationReader = {};
+
+  private readonly dialect = new SqlServerDialect();
+
+  constructor(
+    options: SqlServerConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader
+  );
+  constructor(
+    name: string,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: SqlServerConnectionConfigurationReader
+  );
+  constructor(
+    arg: string | SqlServerConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: SqlServerConnectionConfigurationReader
+  ) {
+    super();
+    if (typeof arg === 'string') {
+      this.name = arg;
+      if (configReader) {
+        this.configReader = configReader;
+      }
+    } else {
+      const {name, ...configReader} = arg;
+      this.name = name;
+      this.configReader = configReader;
+    }
+    if (queryOptionsReader) {
+      this.queryOptionsReader = queryOptionsReader;
+    }
+  }
+
+  private async readQueryConfig(): Promise<RunSQLOptions> {
+    if (this.queryOptionsReader instanceof Function) {
+      return this.queryOptionsReader();
+    } else {
+      return this.queryOptionsReader;
+    }
+  }
+
+  protected async readConfig(): Promise<SqlServerConnectionConfiguration> {
+    if (this.configReader instanceof Function) {
+      return this.configReader();
+    } else {
+      return this.configReader;
+    }
+  }
+
+  get dialectName(): string {
+    return 'SqlServer';
+  }
+
+  public isPool(): this is PooledConnection {
+    return false;
+  }
+
+  public canPersist(): this is PersistSQLResults {
+    return true;
+  }
+
+  public canStream(): this is StreamingConnection {
+    return true;
+  }
+
+  public get supportsNesting(): boolean {
+    return true;
+  }
+
+  protected async getClient(): Promise<ConnectionPool> {
+    const {
+      username: user,
+      password,
+      databaseName: database,
+      port,
+      host = 'localhost',
+    } = await this.readConfig();
+    return connect({
+      user,
+      password,
+      database,
+      port,
+      server: host,
+    });
+  }
+
+  protected async runSqlServerQuery(
+    sqlCommand: string,
+    _pageSize: number,
+    _rowIndex: number,
+    deJSON: boolean
+  ): Promise<MalloyQueryData> {
+    const client = await this.getClient();
+    await client.connect();
+    await this.connectionSetup(client);
+
+    let result = await client.query(sqlCommand);
+    if (Array.isArray(result)) {
+      result = result.pop();
+    }
+    if (deJSON) {
+      for (let i = 0; i < result.rows.length; i++) {
+        result.rows[i] = result.rows[i].row;
+      }
+    }
+    await client.end();
+    return {
+      rows: result.rows as QueryData,
+      totalRows: result.rows.length,
+    };
+  }
+
+  async fetchSelectSchema(
+    sqlRef: SQLSourceRequest
+  ): Promise<SQLSourceDef | string> {
+    const structDef: SQLSourceDef = {
+      type: 'sql_select',
+      ...sqlRef,
+      dialect: this.dialectName,
+      fields: [],
+      name: sqlKey(sqlRef.connection, sqlRef.selectStr),
+    };
+    const tempTableName = `tmp${randomUUID()}`.replace(/-/g, '');
+    const infoQuery = `
+      drop table if exists ${tempTableName};
+      create temp table ${tempTableName} as SELECT * FROM (
+        ${sqlRef.selectStr}
+      ) as x where false;
+      SELECT column_name, c.data_type, e.data_type as element_type
+      FROM information_schema.columns c LEFT JOIN information_schema.element_types e
+        ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)
+          = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier))
+      where table_name='${tempTableName}';
+    `;
+    try {
+      await this.schemaFromQuery(infoQuery, structDef);
+    } catch (error) {
+      return `Error fetching schema for ${structDef.name}: ${error}`;
+    }
+    return structDef;
+  }
+
+  private async schemaFromQuery(
+    infoQuery: string,
+    structDef: StructDef
+  ): Promise<void> {
+    const {rows, totalRows} = await this.runSqlServerQuery(
+      infoQuery,
+      SCHEMA_PAGE_SIZE,
+      0,
+      false
+    );
+    if (!totalRows) {
+      throw new Error('Unable to read schema.');
+    }
+    for (const row of rows) {
+      const SqlServerDataType = row['data_type'] as string;
+      const name = row['column_name'] as string;
+      if (SqlServerDataType === 'ARRAY') {
+        const elementType = this.dialect.sqlTypeToMalloyType(
+          row['element_type'] as string
+        );
+        structDef.fields.push(mkArrayDef(elementType, name));
+      } else {
+        const malloyType = this.dialect.sqlTypeToMalloyType(SqlServerDataType);
+        structDef.fields.push({...malloyType, name});
+      }
+    }
+  }
+
+  async fetchTableSchema(
+    tableKey: string,
+    tablePath: string
+  ): Promise<TableSourceDef | string> {
+    const structDef: StructDef = {
+      type: 'table',
+      name: tableKey,
+      dialect: 'SqlServer',
+      tablePath,
+      connection: this.name,
+      fields: [],
+    };
+    const [schema, table] = tablePath.split('.');
+    if (table === undefined) {
+      return 'Default schema not yet supported in SqlServer';
+    }
+    const infoQuery = `
+      SELECT column_name, c.data_type, e.data_type as element_type
+      FROM information_schema.columns c LEFT JOIN information_schema.element_types e
+        ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)
+          = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier))
+        WHERE table_name = '${table}'
+          AND table_schema = '${schema}'
+    `;
+
+    try {
+      await this.schemaFromQuery(infoQuery, structDef);
+    } catch (error) {
+      return `Error fetching schema for ${tablePath}: ${error.message}`;
+    }
+    return structDef;
+  }
+
+  public async test(): Promise<void> {
+    await this.runSQL('SELECT 1');
+  }
+
+  public async connectionSetup(client: ConnectionPool): Promise<void> {
+    await client.query("SET TIME ZONE 'UTC'");
+  }
+
+  public async runSQL(
+    sql: string,
+    {rowLimit}: RunSQLOptions = {},
+    rowIndex = 0
+  ): Promise<MalloyQueryData> {
+    const config = await this.readQueryConfig();
+
+    return this.runSqlServerQuery(
+      sql,
+      rowLimit ?? config.rowLimit ?? DEFAULT_PAGE_SIZE,
+      rowIndex,
+      true
+    );
+  }
+
+  public async *runSQLStream(
+    sqlCommand: string,
+    {rowLimit, abortSignal}: RunSQLOptions = {}
+  ): AsyncIterableIterator<QueryDataRow> {
+    const client = await this.getClient();
+    const request = client.request();
+    request.stream = true;
+    await client.connect();
+    const rowStream = request.query(sqlCommand);
+    let index = 0;
+    for await (const row of rowStream) {
+      yield row.row as QueryDataRow;
+      index += 1;
+      if (
+        (rowLimit !== undefined && index >= rowLimit) ||
+        abortSignal?.aborted
+      ) {
+        request.cancel();
+        break;
+      }
+    }
+    await client.close();
+  }
+
+  public async estimateQueryCost(_: string): Promise<QueryRunStats> {
+    return {};
+  }
+
+  public async manifestTemporaryTable(sqlCommand: string): Promise<string> {
+    const hash = crypto.createHash('md5').update(sqlCommand).digest('hex');
+    const tableName = `tt${hash}`;
+
+    const cmd = `CREATE TEMPORARY TABLE IF NOT EXISTS ${tableName} AS (${sqlCommand});`;
+    // console.log(cmd);
+    await this.runSqlServerQuery(cmd, 1000, 0, false);
+    return tableName;
+  }
+
+  async close(): Promise<void> {
+    return;
+  }
+}
+
+export class PooledSqlServerConnection
+  extends SqlServerConnection
+  implements PooledConnection
+{
+  private _pool: ConnectionPool | undefined;
+
+  constructor(
+    options: SqlServerConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader
+  );
+  constructor(
+    name: string,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: SqlServerConnectionConfigurationReader
+  );
+  constructor(
+    arg: string | SqlServerConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: SqlServerConnectionConfigurationReader
+  ) {
+    if (typeof arg === 'string') {
+      super(arg, queryOptionsReader, configReader);
+    } else {
+      super(arg, queryOptionsReader);
+    }
+  }
+
+  public isPool(): this is PooledConnection {
+    return true;
+  }
+
+  public async drain(): Promise<void> {
+    await this._pool?.close();
+  }
+
+  async getPool(): Promise<ConnectionPool> {
+    if (!this._pool) {
+      const {
+        username: user,
+        password,
+        databaseName: database,
+        port,
+        host,
+      } = await this.readConfig();
+      this._pool = new ConnectionPool({
+        user,
+        password,
+        database,
+        port,
+        server: host || 'localhost',
+      });
+      this._pool.on('acquire', client => client.query("SET TIME ZONE 'UTC'"));
+    }
+    return this._pool;
+  }
+
+  protected async runSqlServerQuery(
+    sqlCommand: string,
+    _pageSize: number,
+    _rowIndex: number,
+    deJSON: boolean
+  ): Promise<MalloyQueryData> {
+    const pool = await this.getPool();
+    let result = await pool.query(sqlCommand);
+
+    if (Array.isArray(result)) {
+      result = result.pop();
+    }
+    if (deJSON) {
+      for (let i = 0; i < result.rows.length; i++) {
+        result.rows[i] = result.rows[i].row;
+      }
+    }
+    return {
+      rows: result.rows as QueryData,
+      totalRows: result.rows.length,
+    };
+  }
+
+  public async *runSQLStream(
+    sqlCommand: string,
+    {rowLimit, abortSignal}: RunSQLOptions = {}
+  ): AsyncIterableIterator<QueryDataRow> {
+    let index = 0;
+    // This is a strange hack... `this.pool.query(query)` seems to return the wrong
+    // type. Because `query` is a `QueryStream`, the result is supposed to be a
+    // `QueryStream` as well, but it's not. So instead, we get a client and call
+    // `client.query(query)`, which does what it's supposed to.
+    const pool = await this.getPool();
+    const client = await pool.connect();
+    const request = client.request();
+    request.stream = true;
+    const resultStream = request.query(sqlCommand);
+    for await (const row of resultStream) {
+      yield row.row as QueryDataRow;
+      index += 1;
+      if (
+        (rowLimit !== undefined && index >= rowLimit) ||
+        abortSignal?.aborted
+      ) {
+        request.cancel();
+        break;
+      }
+    }
+    client.close();
+  }
+
+  async close(): Promise<void> {
+    await this.drain();
+  }
+}
