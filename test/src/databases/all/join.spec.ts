b27cc11d71067f1474b43d15b5918a54c8971e67
Michael Toy <66150587+mtoy-googly-moogly@users.noreply.github.com>
2025-04-14 12:01:46 -1000

    Investigation (and fix I hope) for finding a missing join dependency (#2248)
    * try to track down a missing join dependency
    
    * add group by array test
    
    * enable double cross join aray test
    
    * skip test on fake array dialects
    
    * merge main
    
    * spell aggggregate properly
    
    * run the failing tests
    
    * skip split tests
    
    * move the tests to presto only
    
    * fix and test for joins references in non basic results
    
    * attempt to run the correct tests
    
    * put test in right place
    
    * remove mis-placed test
    
    * all hail our benevolent robot protectors
    
    * some dialects are precious snowflakes,like snowflake
    
    Signed-off-by: Jan Guzman <janfrancisco19@gmail.com>

--- test/src/databases/all/join.spec.ts
+++ test/src/databases/all/join.spec.ts
@@ -302,4 +302,15 @@ describe.each(runtimes.runtimeList)('%s', (databaseName, runtime) => {
       }
     `).malloyResultMatches(joinModel, {c: 0});
   });
+
+  it.when(runtime.dialect.nestedArrays)(
+    'finds join dependency in non basic atomic fields',
+    async () => {
+      await expect(`
+      run: ${databaseName}.sql("SELECT 1 as n")
+        extend { dimension: a1 is [[1]], a2 is [[2]] }
+        -> { select: pick_a1 is pick a1.each when true else a2.each }
+      `).matchesRows(runtime, {pick_a1: [1]});
+    }
+  );
 });
