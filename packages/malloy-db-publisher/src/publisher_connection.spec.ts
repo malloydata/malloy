f938827c782fa097c4152bcd9bf37b89f915816f
Kyle Nesbit <kjnesbit@gmail.com>
2025-04-13 21:39:14 -0600

    Improve publisher connection tests.
    Signed-off-by: Kyle Nesbit <kjnesbit@gmail.com>
    Signed-off-by: Jan Guzman <janfrancisco19@gmail.com>

--- packages/malloy-db-publisher/src/publisher_connection.spec.ts
+++ packages/malloy-db-publisher/src/publisher_connection.spec.ts
@@ -11,9 +11,9 @@ describe('db:Publisher', () => {
   let conn: PublisherConnection;
   let runtime: malloy.Runtime;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     conn = await PublisherConnection.create('bigquery', {
-      connectionUri: 'http://localhost:4001/projects/home',
+      connectionUri: 'http://localhost:4000/api/v0/projects/home/connections/bigquery',
     });
     const files = {
       readURL: async (url: URL) => {
@@ -27,8 +27,50 @@ describe('db:Publisher', () => {
     });
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await conn.close();
+  });
+
+  it('tests the connection', async () => {
+    await conn.test();
+  });
+
+  it('correctly identifies the dialect', () => {
+    expect(conn.dialectName).toBe('standardsql');
+  });
+
+  it('correctly identifies the connection as a pooled connection', () => {
+    expect(conn.isPool()).toBe(false);
+  });
+
+  it('correctly identifies the connection as a streaming connection', () => {
+    expect(conn.canStream()).toBe(true);
+  });
+
+  it('correctly identifies the connection as a persistSQLResults connection', () => {
+    expect(conn.canPersist()).toBe(true);
+  });
+
+  it('fetches the table schema', async () => {
+    const schema = await conn.fetchTableSchema('bigquery', 'bigquery-public-data.hacker_news.full');
+    expect(schema.type).toBe('table');
+    expect(schema.dialect).toBe('standardsql');
+    expect(schema.tablePath).toBe('bigquery-public-data.hacker_news.full');
+    expect(schema.fields.length).toBe(14);
+    expect(schema.fields[0].name).toBe('title');
+    expect(schema.fields[0].type).toBe('string');
+  });
+
+  it('fetches the sql source schema', async () => {
+    const schema = await conn.fetchSelectSchema({
+      connection: 'bigquery',
+      selectStr: 'SELECT * FROM bigquery-public-data.hacker_news.full',
+    });
+    expect(schema.type).toBe('sql_select');
+    expect(schema.dialect).toBe('standardsql');
+    expect(schema.fields.length).toBe(14);
+    expect(schema.fields[0].name).toBe('title');
+    expect(schema.fields[0].type).toBe('string');
   });
 
   it('runs a SQL query', async () => {
@@ -39,30 +81,34 @@ describe('db:Publisher', () => {
   it('runs a Malloy query', async () => {
     const sql = await runtime
       .loadModel("source: stories is bigquery.table('bigquery-public-data.hacker_news.full')")
-      .loadQuery('run:  stories -> { aggregate: cnt is count() }')
+      .loadQuery('run:  stories -> { aggregate: cnt is count() group_by: `by` order_by: cnt desc limit: 10 }')
       .getSQL();
     const res = await conn.runSQL(sql);
-    expect(res.totalRows).toBe(55);
+    console.log(res);
+    expect(res.totalRows).toBe(10);
     let total = 0;
     for (const row of res.rows) {
       total += +(row['cnt'] ?? 0);
     }
-    expect(total).toBe(3540);
-
-    // if we request for a smaller rowLimit we should get fewer rows
-    const res_limited = await conn.runSQL(sql, { rowLimit: 10 });
-    expect(res_limited.totalRows).toBe(10);
+    expect(total).toBe(1836679);
   });
 
   it('runs a Malloy query on an sql source', async () => {
     const sql = await runtime
       .loadModel(
-        "source: aircraft is bigquery.sql('SELECT * FROM bigquery-public-data.hacker_news.full')"
+        "source: stories is bigquery.sql('SELECT * FROM bigquery-public-data.hacker_news.full')"
       )
-      .loadQuery('run:  aircraft -> { aggregate: cnt is count() }')
+      .loadQuery('run:  stories -> { aggregate: cnt is count() group_by: `by` order_by: cnt desc limit: 20 }')
       .getSQL();
     const res = await conn.runSQL(sql);
-    expect(res.rows.length).toBe(1);
-    expect(res.rows[0]['cnt']).toBe(3599);
-  }); 1
+    expect(res.totalRows).toBe(20);
+    expect(res.rows[0]['cnt']).toBe(1346912);
+  });
+
+  it('get temporary table name', async () => {
+    const sql = 'SELECT 1 as T';
+    const tempTableName = await conn.manifestTemporaryTable(sql);
+    expect(tempTableName).toBeDefined();
+    expect(tempTableName.startsWith('lofty-complex-452701')).toBe(true);
+  });
 });
