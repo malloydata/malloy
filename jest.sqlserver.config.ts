a488fad57800973547d12ea3ad43c7bdc901ece5
vitorelourenco <vitor.registros@lourencos.net>
2025-04-16 15:27:26 -0300

    Run all tests in CI

--- jest.sqlserver.config.ts
+++ jest.sqlserver.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   ...require('./jest.config.ts'),
   roots: [
     '<rootDir>/packages/malloy-db-sqlserver/',
-    // '<rootDir>/test/src/databases/all/',
+    '<rootDir>/test/src/databases/all/',
     '<rootDir>/test/src/databases/sqlserver/',
   ],
 };
