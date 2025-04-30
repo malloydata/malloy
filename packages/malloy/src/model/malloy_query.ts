c6bccc08bbb4f4281f49d02f9d44ac273b4700d9
vitorelourenco <vitor.registros@lourencos.net>
2025-04-30 11:59:49 -0300

    Fix base replace

--- packages/malloy/src/model/malloy_query.ts
+++ packages/malloy/src/model/malloy_query.ts
@@ -3321,7 +3321,7 @@ class QueryQuery extends QueryField {
 
     s += this.generateSQLJoins(stageWriter);
     s += this.generateSQLFilters(this.rootResult, 'where').sql('where');
-    s += `) as base__${uuidv4()}\n`;
+    s += `) as base__${uuidv4().replace(/-/g, '')}\n`;
 
     // group by
     if (this.firstSegment.type === 'reduce') {
@@ -3823,7 +3823,7 @@ class QueryQuery extends QueryField {
       s += `WHERE ${where}\n`;
     }
 
-    s += `) as base__${uuidv4()}\n`;
+    s += `) as base__${uuidv4().replace(/-/g, '')}\n`;
 
     if (dimensionIndexes.length > 0) {
       // TODO (vitor): Not sure about dimensionNames here
@@ -4326,7 +4326,7 @@ class QueryQueryIndexStage extends QueryQuery {
 
     s += this.generateSQLFilters(this.rootResult, 'where').sql('where');
 
-    s += `) as base__${uuidv4()}\n`;
+    s += `) as base__${uuidv4().replace(/-/g, '')}\n`;
 
     // TODO (vitor): Sort out this here with the malloy team. Code smell ahead.
     if (dialect.orderByClause === 'output_name') {
