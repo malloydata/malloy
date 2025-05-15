2e3db33b48a1dc6ee4e268b614153b776b68e601
vitorelourenco <vitor.registros@lourencos.net>
2025-05-15 11:12:15 -0300

    Try string agg distinct for tsql

--- packages/malloy/src/dialect/tsql/dialect_functions.ts
+++ packages/malloy/src/dialect/tsql/dialect_functions.ts
@@ -35,9 +35,47 @@ const string_agg: OverloadedDefinitionBlueprint = {
   },
 };
 
-// TODO (vitor): Discuss the possibility of having string_agg_distinct here.
+// TODO (vitor):Really not sure about this...
+const string_agg_distinct: OverloadedDefinitionBlueprint = {
+  default_separator: {
+    ...string_agg['default_separator'],
+    isSymmetric: true,
+    supportsOrderBy: 'only_default',
+    impl: {
+      // T-SQL requires DISTINCT to be pre-applied before aggregation
+      sql: `
+        (
+          SELECT STRING_AGG(\${value}, ',')
+          FROM (
+            SELECT DISTINCT \${value}
+            \${order_by ? 'ORDER BY \${order_by}' : ''}
+          )
+        )
+      `,
+      defaultOrderByArgIndex: 0,
+    },
+  },
+  with_separator: {
+    ...string_agg['with_separator'],
+    isSymmetric: true,
+    supportsOrderBy: 'only_default',
+    impl: {
+      sql: `
+        (
+          SELECT STRING_AGG(\${value}, \${separator})
+          FROM (
+            SELECT DISTINCT \${value}
+            \${order_by ? 'ORDER BY \${order_by}' : ''}
+          )
+        )
+      `,
+      defaultOrderByArgIndex: 0,
+    },
+  },
+};
 
 export const TSQL_DIALECT_FUNCTIONS: DefinitionBlueprintMap = {
+  string_agg_distinct,
   string_agg,
   ...def('repeat', {'str': 'string', 'n': 'number'}, 'string'),
   ...def('reverse', {'str': 'string'}, 'string'),
