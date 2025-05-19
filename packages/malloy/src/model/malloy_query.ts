957e352f4641838b67432ce2a7cfb94bbd27b2d6
vitorelourenco <vitor.registros@lourencos.net>
2025-05-19 16:45:11 -0300

    Use formatLimit function

--- packages/malloy/src/model/malloy_query.ts
+++ packages/malloy/src/model/malloy_query.ts
@@ -157,6 +157,11 @@ const SQL_CONST_EXPR = new RegExp(
   `(?:${FLOAT_EXPR.source})|(?:${SQL_STR_LITERAL_EXPR.source})`
 );
 
+function genPlaceholder() {
+  // TODO (vitor): Idk about this idea. Also one uuid should be fine
+  return (uuidv4() + uuidv4()).replace(/-/g, '');
+}
+
 function pathToCol(path: string[]): string {
   return path.map(el => encodeURIComponent(el)).join('/');
 }
@@ -3206,6 +3211,25 @@ class QueryQuery extends QueryField {
     return ret;
   }
 
+  formatLimit(
+    sqlStr: string,
+    limitStyle: string,
+    limitPlaceholder: string,
+    limit?: string | number | null
+  ): string {
+    if (limit) {
+      if (limitStyle === 'limit') {
+        return sqlStr.replace(limitPlaceholder, ` LIMIT ${limit}\n`);
+      } else if (limitStyle === 'top') {
+        return sqlStr.replace(limitPlaceholder, ` TOP ${limit}\n`);
+      } else {
+        throw new Error(`limitStyle ${limitStyle} not implemented`);
+      }
+    } else {
+      return sqlStr.replace(limitPlaceholder, '');
+    }
+  }
+
   generateSQLJoins(stageWriter: StageWriter): string {
     let s = '';
     // get the first value from the map (weird, I know)
@@ -3222,14 +3246,21 @@ class QueryQuery extends QueryField {
         const limit =
           (!isRawSegment(this.firstSegment) && this.firstSegment.limit) || null;
 
-        // TODO (vitor): double check this
-        // maybe we don't want that order by here.
+        const limitPlaceholder = genPlaceholder();
+
         structSQL = stageWriter.addStage(
-          `SELECT${
-            this.parent.dialect.limitStyle === 'top' ? ` TOP ${limit}` : ''
+          `SELECT ${
+            this.parent.dialect.limitStyle === 'top' ? limitPlaceholder : ''
           } * from ${structSQL} as x ${
-            this.parent.dialect.limitStyle === 'limit' ? `LIMIT ${limit}\n` : ''
+            this.parent.dialect.limitStyle === 'limit' ? limitPlaceholder : ''
           }`
+        );
+
+        structSQL = this.formatLimit(
+          structSQL,
+          this.parent.dialect.limitStyle,
+          limitPlaceholder,
+          limit
         );
       }
     }
