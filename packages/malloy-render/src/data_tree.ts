69aa5983443859efe6467e49f0a5c8258bebec0d
Speros Kokenes <speros@meta.com>
2025-05-19 15:38:46 -0400

    fix: lint and types

--- packages/malloy-render/src/data_tree.ts
+++ packages/malloy-render/src/data_tree.ts
@@ -1302,7 +1302,7 @@ export class RepeatedRecordCell extends ArrayCell {
     plugins: RenderPlugin[] = [],
     registry?: FieldRegistry
   ) {
-    super(cell, field, parent, plugins, registry);
+    super(cell, field, parent);
     this.plugins = plugins;
     this.registry = registry;
     this.rows = this.values as RecordCell[];
