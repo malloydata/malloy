7cb707a415bcfdbfa03edbd89308e48aae4669f6
Will Scullin <scullin@meta.com>
2025-05-27 13:20:54 -0700

    Explore render component type

--- packages/malloy-render/src/component/render-webcomponent.ts
+++ packages/malloy-render/src/component/render-webcomponent.ts
@@ -1,3 +1,11 @@
 import registerWebComponent from './register-webcomponent';
+import type {MalloyRenderProps} from './render';
+export type {MalloyRenderProps} from './render';
 
 registerWebComponent({});
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'malloy-render': HTMLElement & MalloyRenderProps;
+  }
+}
