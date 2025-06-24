be0c26f7a572a15b8b3e348e1aaf7e4a9bd08cce
Will Scullin <scullin@meta.com>
2025-06-24 10:36:21 -0700

    Use jest projects instead of separate config files.

--- scripts/ci-test-sanity-check.sh
+++ scripts/ci-test-sanity-check.sh
@@ -7,16 +7,13 @@ MALLOY_ROOT=$(cd $(dirname $0)/..; pwd)
 all_test_file=/tmp/mly_all_test.$$
 ci_test_file=/tmp/mly_ci_test.$$
 cd $MALLOY_ROOT
-npx jest --listTests | sort > $all_test_file
-for ci_test in jest.*.config.ts; do
-  npx jest --config $ci_test --listTests >> $ci_test_file
-done
-sort -u $ci_test_file -o $ci_test_file
+find $MALLOY_ROOT/packages $MALLOY_ROOT/test -name '*.spec.ts' -or -name '*.spec.tsx' | sort > $all_test_file
+npx jest --listTests | sort > $ci_test_file
 diff $all_test_file $ci_test_file
 status=$?
 rm -rf $all_test_file $ci_test_file
 if [ $status -ne 0 ]; then
-  echo "!!!!!!! jest.config.* CI configurations must up dated, some tests are missing !!!!!!!!"
+  echo "!!!!!!! jest.config.ts project configurations must up dated, some tests are missing !!!!!!!!"
   exit 1;
 fi
 exit 0
