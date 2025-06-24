1c0c08695e897ca35cdfdb2ab5ee42d65ed4eabb
Andy Reding <49730013+areding99@users.noreply.github.com>
2025-06-24 12:49:37 -0700

    sourceID -> componentID + move type def (#2399)
    * sourceID -> componentID + move type def
    * make backwards compatible
    
    ---------
    
    Signed-off-by: Andy Reding <andrewreding@meta.com>

--- packages/malloy/src/model/malloy_types.ts
+++ packages/malloy/src/model/malloy_types.ts
@@ -1276,6 +1276,15 @@ export function isBaseTable(def: StructDef): def is SourceDef {
 
 export type StructDef = SourceDef | RecordDef | ArrayDef;
 
+export type SourceComponentInfo =
+  | {type: 'table'; tableName: string; componentID?: string; sourceID?: string}
+  | {
+      type: 'sql';
+      selectStatement: string;
+      componentID?: string;
+      sourceID?: string;
+    };
+
 // "NonAtomic" are types that a name lookup or a computation might
 // have which are not AtomicFieldDefs. I asked an AI for a word for
 // for "non-atomic" and even the AI couldn't think of the right word.
