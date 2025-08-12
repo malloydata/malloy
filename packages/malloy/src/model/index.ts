b8bf0cbe519a64a9649638e4b5b71290723afdfc
Michael Toy <the.michael.toy@gmail.com>
2025-08-12 12:28:51 -0700

    updates comments and add remediation
    DCO Remediation Commit for Michael Toy <the.michael.toy@gmail.com>
    
    I, Michael Toy <the.michael.toy@gmail.com>, hereby add my Signed-off-by to this commit: f2558a32adac52943638d6df4df56a6c39fcbdaa
    I, Michael Toy <the.michael.toy@gmail.com>, hereby add my Signed-off-by to this commit: 03d7dd55af36fdd75a935d3b99becf09e579f6e6
    I, Michael Toy <the.michael.toy@gmail.com>, hereby add my Signed-off-by to this commit: ec36449447a5c73043a67feed6300b4b4c1acf71
    
    Signed-off-by: Michael Toy <the.michael.toy@gmail.com>

--- packages/malloy/src/model/index.ts
+++ packages/malloy/src/model/index.ts
@@ -30,6 +30,10 @@ import {QueryQuery} from './query_query';
 import {FieldInstanceField} from './field_instance';
 import {QueryModelImpl} from './query_model_impl';
 
+// We have a circular dependency issue, and this is the minimal
+// dependency injection needed to get around it. Ideally, we would
+// like to avoid this, and I think a thoughtful pass through
+// FieldInstance and QueryField might eliminate the need for this.
 function getLookupFun(
   mri: ModelRootInterface
 ): (name: string) => QueryStruct | undefined {
@@ -39,7 +43,6 @@ function getLookupFun(
   return () => undefined;
 }
 
-// Register the functions which break difficult circularity
 FieldInstanceField.registerExpressionCompiler(exprToSQL);
 QueryStruct.registerTurtleFieldMaker((field, parent) =>
   QueryQuery.makeQuery(
@@ -51,6 +54,8 @@ QueryStruct.registerTurtleFieldMaker((field, parent) =>
   )
 );
 
+// Note the internal vs. external naming of QueryModel, this is another
+// circular dependency problem which might have a better fix in the future.
 export {QueryField, QueryStruct, QueryQuery, QueryModelImpl as QueryModel};
 
 export {getResultStructDefForQuery} from './query_model_impl';
